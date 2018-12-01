6aaae1b36e95178de4f04fb21dc8e65fa9816c51
David Bell <david.bell@gmail.com>
2018-12-01 11:08:39 +0000

    2018 day1

---
+++ 2018/day1/part2_golf.pl
@@ -0,0 +1,1 @@
+@f=<>;$r=0;while(1){foreach(@f){$r+=$_;if($s{$r}){print$r;exit}$s{$r}=1}}
---
+++ 2018/day1/run.sh
@@ -0,0 +1,6 @@
+./part1.pl
+./part2.pl
+cat input.txt | perl part1_golf.pl
+echo
+cat input.txt | perl part2_golf.pl
+echo
