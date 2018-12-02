773e0200cb84b5c7f2d5977c2b87e9068772946a
David Bell <david.bell@gmail.com>
2018-12-02 11:46:05 +0000

    chomp to remove newlines from input

--- 2018/day2/run.pl
+++ 2018/day2/run.pl
@@ -6,6 +6,7 @@ use Inventory qw(checksum find_similar);
 open my $fh, '<', 'input.txt';
 my @boxes = <$fh>;
 close $fh;
+chomp($_) foreach @boxes;
 
 print checksum(@boxes) . "\n";
 print find_similar(@boxes) . "\n";
