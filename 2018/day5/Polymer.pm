4af1f8c34e5443a5731ceda8c0dd9133d7116b1e
David Bell <david.bell@gmail.com>
2018-12-05 13:00:25 +0000

    day5 part2

--- 2018/day5/Polymer.pm
+++ 2018/day5/Polymer.pm
@@ -3,12 +3,12 @@ use strict;
 use warnings;
 
 use parent "Exporter";
-our @EXPORT_OK = qw(reduction);
+our @EXPORT_OK = qw(reduction find_best_after_removal);
 
-use constant AB => qw/abcdefghijklmnopqrstuvwxyz/;
+my @ab = split//,'abcdefghijklmnopqrstuvwxyz';
 
 my $search;
-foreach my $l (split//,&AB) {
+foreach my $l (@ab) {
 	$search .= '|' if $search;
 	$search .= lc($l) . uc($l) . '|' . uc($l) . lc($l);
 }
@@ -20,4 +20,21 @@ sub reduction {
 		last unless $polymer =~ s/$search//g;
 	}
 	return $polymer;
+}
+
+sub find_best_after_removal {
+	my ($polymer) = @_;
+
+	my $best = $polymer;
+
+	for my $l (@ab) {
+		my $stripped = $polymer;
+		$stripped =~ s/$l//ig;
+		$stripped = reduction($stripped);
+		if (length($stripped) < length($best)) {
+			$best = $stripped;
+		}
+	}
+
+	return $best;
 }
--- 2018/day5/run.pl
+++ 2018/day5/run.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Polymer qw(reduction);
+use Polymer qw(reduction find_best_after_removal);
 
 open my $fh, '<', 'input.txt';
 chomp(my $polymer = <$fh>);
 close $fh;
 
-$polymer = reduction($polymer);
-print length($polymer) . "\n";
+my $reduced = reduction($polymer);
+print length($reduced) . "\n";
+
+my $best = find_best_after_removal($polymer);
+print length($best) . "\n";
