5f406e9cf160496d1d119e5ba323d3857d762a84
David Bell <david.bell@gmail.com>
2018-12-05 08:42:45 +0000

    removed repeat call

--- 2018/day5/run.pl
+++ 2018/day5/run.pl
@@ -8,11 +8,4 @@ chomp(my $polymer = <$fh>);
 close $fh;
 
 $polymer = reduction($polymer);
-print "$polymer\n";
 print length($polymer) . "\n";
-
-$polymer = reduction($polymer);
-print "$polymer\n";
-print length($polymer) . "\n";
-
-
