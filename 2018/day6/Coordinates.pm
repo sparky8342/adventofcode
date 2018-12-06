ce4cff8f29e84bbfade6e10e8a9a7847679562fc
David Bell <david.bell@gmail.com>
2018-12-06 23:30:04 +0000

    day6 part2

--- 2018/day6/Coordinates.pm
+++ 2018/day6/Coordinates.pm
@@ -3,10 +3,10 @@ use strict;
 use warnings;
 
 use parent "Exporter";
-our @EXPORT_OK = qw(largest_area);
+our @EXPORT_OK = qw(find_areas);
 
-sub largest_area {
-	my @data = @_;
+sub find_areas {
+	my ($safe_distance,@data) = @_;
 
 	my @points;
 	foreach my $entry (@data) {
@@ -25,16 +25,21 @@ sub largest_area {
 
 	my %area;
 	my %ignore;
+
+	my $safe_region = 0;
+
 	foreach my $x ($startx..$endx) {
 		foreach my $y ($startx..$endy) {
 			my ($smallest_dist,$smallest_point);
 
 			my %distances;
+			my $distance_sum = 0;
 
 			for (my $i = 0; $i < @points; $i++) {
 				my ($pointx,$pointy) = @{$points[$i]};
 				my $dist = abs($x - $pointx) + abs($y - $pointy);
 				$distances{$i} = $dist;
+				$distance_sum += $dist;
 			}
 
 			my @closest_points = sort { $distances{$a} <=> $distances{$b} } keys %distances;
@@ -47,11 +52,15 @@ sub largest_area {
 					$area{$closest_points[0]}++;
 				}
 			}
+
+			if ($distance_sum < $safe_distance) {
+				$safe_region++;
+			}
 		}
 	}
 
 	delete($area{$_}) foreach keys %ignore;
 
 	my $l = (sort { $b <=> $a } values %area)[0];
-	return $l;
+	return ($l,$safe_region);
 }
