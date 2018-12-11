f7ff26cf3c6473ef449e174d24a710389361b041
David Bell <david.bell@gmail.com>
2018-12-11 20:33:25 +0000

    reworking to only calculate each square once

--- 2018/day11/FuelGrid.pm
+++ 2018/day11/FuelGrid.pm
@@ -5,33 +5,50 @@ use warnings;
 use parent "Exporter";
 our @EXPORT_OK = qw(find_best_square);
 
+$| = 1;
+
 sub find_best_square {
 	my ($serial) = @_;
  
 	my $max_power = 0;
 	my $max_x;
 	my $max_y;
+	my $max_size;
+
+	my @grid;
+	for my $x (1..300) {
+		my $rack_id = $x + 10;
+		for my $y (1..300) {
+			my $pl = $rack_id * $y;
+			$pl += $serial;
+			$pl *= $rack_id;
+			$pl = int($pl/100);
+			$pl = $pl % 10;	
+			$pl -= 5;
+			$grid[$x][$y] = $pl;
+		}
+	}
 
 	for my $x (1..298) {
 		for my $y (1..298) {
-			my $power = 0;
-			for my $dx (0..2) {
-				my $rack_id = ($x + $dx) + 10;
-				for my $dy (0..2) {
-					my $pl = $rack_id * ($y + $dy);
-					$pl += $serial;
-					$pl *= $rack_id;
-					$pl = int($pl/100);
-					$pl = $pl % 10;	
-					$pl -= 5;
-					$power += $pl;
+			print "$x $y\n";
+			#my $size_limit = 300 - ($x > $y ? $x : $y);
+			#foreach my $size (1..$size_limit) {
+			foreach my $size (2..2) {
+				my $power = 0;
+				for my $dx (0..$size) {
+					for my $dy (0..$size) {
+						$power += $grid[$x+$dx][$y+$dy];
+					}
 				}
-			}
-			if ($power > $max_power) {
-				print "$max_power\n";
-				$max_power = $power;
-				$max_x = $x;
-				$max_y = $y;
+
+				if ($power > $max_power) {
+					print "$max_power\n";
+					$max_power = $power;
+					$max_x = $x;
+					$max_y = $y;
+					$max_size = $size;
+				}
 			}
 		}
 	}
