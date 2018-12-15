5741c046dbb2e666f1f19adbed4dd386ae8d0314
David Bell <david.bell@gmail.com>
2018-12-15 01:54:10 +0000

    fix

--- 2018/day14/Recipes.pm
+++ 2018/day14/Recipes.pm
@@ -12,7 +12,7 @@ sub make {
 	my $e1 = 0;
 	my $e2 = 1;
 
-	for (1..$n + 10) {
+	for (1..$n + 9) {
 		push @r, split//,$r[$e1] + $r[$e2];
 		$e1 = ($e1 + $r[$e1] + 1) % @r;
 		$e2 = ($e2 + $r[$e2] + 1) % @r;
