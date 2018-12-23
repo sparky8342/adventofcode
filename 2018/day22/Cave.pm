6b7aad584cb758d97be146ab2da939395e953209
David Bell <david.bell@gmail.com>
2018-12-23 01:52:11 +0000

    adjustments for part2

--- 2018/day22/Cave.pm
+++ 2018/day22/Cave.pm
@@ -12,24 +12,29 @@ sub risk_level {
 
 	my $geo_grid = [];
 	my $ero_grid = [];
+	my $type_grid = [];
 	my $risk = 0;
 	$geo_grid->[0][0] = 0;
 	$ero_grid->[0][0] = $depth % 20183;
-	$risk += $ero_grid->[0][0] % 3;
+	$type_grid->[0][0] = $ero_grid->[0][0] % 3;
+	$risk += $type_grid->[0][0];
 	$geo_grid->[$target_x][$target_y] = 0;
 	$ero_grid->[$target_x][$target_y] = $depth % 20183;
-	$risk += $ero_grid->[$target_x][$target_y] % 3;
+	$type_grid->[$target_x][$target_y] = $ero_grid->[$target_x][$target_y] % 3;
+	$risk += $type_grid->[$target_x][$target_y];
 
-	for my $x (1..$target_x) {
+	for my $x (1..$target_x + 10) {
 		$geo_grid->[$x][0] = $x * 16807;
 		$ero_grid->[$x][0] = ($geo_grid->[$x][0] + $depth) % 20183;
-		$risk += $ero_grid->[$x][0] % 3;
+		$type_grid->[$x][0] = $ero_grid->[$x][0] % 3;
+		$risk += $type_grid->[$x][0] if $x <= $target_x;
 	}
 
-	for my $y (1..$target_y) {
+	for my $y (1..$target_y + 10) {
 		$geo_grid->[0][$y] = $y * 48271;
 		$ero_grid->[0][$y] = ($geo_grid->[0][$y] + $depth) % 20183;
-		$risk += $ero_grid->[0][$y] % 3;
+		$type_grid->[0][$y] = $ero_grid->[0][$y] % 3;
+		$risk += $type_grid->[0][$y] if $y <= $target_y;
 	}	
 
 	for my $x (1..$target_x) {
@@ -37,9 +42,10 @@ sub risk_level {
 			last if $x == $target_x && $y == $target_y;
 			$geo_grid->[$x][$y] = $ero_grid->[$x-1][$y] * $ero_grid->[$x][$y-1]; 
 			$ero_grid->[$x][$y] = ($geo_grid->[$x][$y] + $depth) % 20183;
-			$risk += $ero_grid->[$x][$y] % 3;
+			$type_grid->[$x][$y] = $ero_grid->[$x][$y] % 3;
+			$risk += $type_grid->[$x][$y] if $x <= $target_x && $y <= $target_y;
 		}
 	}
 
-	return $risk;
+	return ($risk,$type_grid);
 }
