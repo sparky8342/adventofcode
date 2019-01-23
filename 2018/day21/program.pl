1cb1a2a8595c6cb9b65b227259edc8b5f00c8a3b
David Bell <david.bell@gmail.com>
2019-01-23 00:56:38 +0000

    more tidy

--- 2018/day21/program.pl
+++ 2018/day21/program.pl
@@ -13,47 +13,47 @@ if ($a == 72) {
 
 	$a = 0;
 
-	L3:
+	while (1) {
 
-	$b = $a | 65536;
-	$a = 16123384;
+		$b = $a | 65536;
+		$a = 16123384;
 
-	L2:
+		L2:
 
-	$c = $b & 255;
-	$a = $a + $c;
-	$a = $a & 16777215;
-	$a = $a * 65899;
-	$a = $a & 16777215;
+		$c = $b & 255;
+		$a = $a + $c;
+		$a = $a & 16777215;
+		$a = $a * 65899;
+		$a = $a & 16777215;
 
 
-	if ($b < 256) {
-		# line 16
-		if ($in == $a) {
-			# end
-			print "$a $b $c $d\n";
-			exit;
+		if ($b < 256) {
+			# line 16
+			if ($in == $a) {
+				# end
+				print "$a $b $c $d\n";
+				exit;
+			}
+			else {
+				next;
+			}	
 		}
 		else {
-			goto L3;		
-		}	
-	}
+			$c = 0;
 
-	else {
-		$c = 0;
+			while (1) {
+				$d = $c + 1;
+				$d *= 256;
 
-		while (1) {
-			$d = $c + 1;
-			$d *= 256;
+				print "$a $b $c $d\n";
 
-			print "$a $b $c $d\n";
+				if ($d > $b) {
+					$b = $c;
+					goto L2;
+				}
 
-			if ($d > $b) {
-				$b = $c;
-				goto L2;
+				$c++;
 			}
-
-			$c++;
 		}	
 	}
 
