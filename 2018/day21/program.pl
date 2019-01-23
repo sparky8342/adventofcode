d1f9aa035fd306b1a1dcf2f6c69f2cfbf61844c9
David Bell <david.bell@gmail.com>
2019-01-23 13:10:20 +0000

    Modified to find the answer for part1 and 2. This is done by keeping track of all possible answers that could end the program, and how many instructions it took to reach them. Then the one with the least instructions and the one with the most are the answers.

--- 2018/day21/program.pl
+++ 2018/day21/program.pl
@@ -2,50 +2,81 @@
 use strict;
 use warnings;
 
-my $in = 12980435;
+# set a value that won't cause the program to end normally
+my $in = 1;
 
-my ($a,$b,$c,$d);
+my ($x,$y,$c,$d);
 
-$a = 0;
+$x = 0;
+
+# used to keep track of possible answers
+my %possible = ();
+
+# count of instructions executed (roughly)
+my $ins_count = 0;
 
 L1:
 while (1) {
 
-	$b = $a | 65536;
-	$a = 16123384;
+	$y = $x | 65536;
+	$x = 16123384;
+
+	$ins_count += 2;
 
 	L2:
 	while (1) {
 
-		$c = $b & 255;
-		$a = $a + $c;
-		$a = $a & 16777215;
-		$a = $a * 65899;
-		$a = $a & 16777215;
+		$c = $y & 255;
+		$x = $x + $c;
+		$x = $x & 16777215;
+		$x = $x * 65899;
+		$x = $x & 16777215;
 
+		$ins_count += 5;
 
-		if ($b < 256) {
-			# line 16
-			if ($in == $a) {
-				# end
-				print "$a $b $c $d\n";
+		if ($y < 256) {
+
+			# if we see a possible answer repeat, stop and find the answers
+			# that took the fewest and most instructions to reach
+			if (exists($possible{$x})) {
+				my %h;
+				foreach my $k (keys %possible) {
+					$h{$possible{$k}} = $k;
+				}
+
+				my @sorted = sort { $b <=> $a } keys %h;
+				print "part1: " . $h{$sorted[-1]} . "\n";
+				print "part2: " . $h{$sorted[0]} . "\n";
+				exit;
+			}
+
+			# record how many instructions to reach this answer
+			$possible{$x} = $ins_count;
+
+			if ($in == $x) {
+				print "$x $y $c $d\n";
 				exit;
 			}
 			else {
+				$ins_count += 2;
 				next L1;
 			}	
 		}
 		else {
 			$c = 0;
 
+			$ins_count++;
+
 			while (1) {
 				$d = $c + 1;
 				$d *= 256;
 
-				print "$a $b $c $d\n";
+				$ins_count += 4;
 
-				if ($d > $b) {
-					$b = $c;
+				#print "$x $y $c $d\n";
+
+				if ($d > $y) {
+					$y = $c;
 					next L2;
 				}
 
@@ -55,4 +86,3 @@ while (1) {
 		}
 	}	
 }
-
