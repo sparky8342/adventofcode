06fe3215d9a2b7c21c800d0b0b428bc2441b1b5f
David Bell <david.bell@gmail.com>
2019-12-01 11:59:07 +0000

    removed unneeded statement

--- 2019/day1/day1_golf.pl
+++ 2019/day1/day1_golf.pl
@@ -1,1 +1,1 @@
-@m=<>;$t=0;map{$t+=int($_/3)-2}@m;print$t
+@m=<>;map{$t+=int($_/3)-2}@m;print$t
--- 2019/day1/day2_golf.pl
+++ 2019/day1/day2_golf.pl
@@ -1,1 +1,1 @@
-@m=<>;$t=0;map{while(1){$_=int($_/3)-2;if($_<0){last};$t+=$_}}@m;print$t
+@m=<>;map{while(1){$_=int($_/3)-2;if($_<0){last};$t+=$_}}@m;print$t
