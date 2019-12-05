e349a055b19d1ea0d21b7ab597a4215842c7bdc9
David Bell <david.bell@gmail.com>
2019-12-05 21:58:02 +0000

    now works for part 2

--- 2019/day5/day5.pl
+++ 2019/day5/day5.pl
@@ -4,6 +4,9 @@ use warnings;
 
 use Term::ReadKey;
 
+# part 1, enter 1
+# part 2, enter 5
+
 ReadMode 'cbreak';
 
 END {
@@ -13,6 +16,8 @@ END {
 my $pos;
 my @p;
 my $exit = 0;
+
+# It's like the synacor challenge, except even messier code
 
 my %commands = (
 	1 => {
@@ -37,6 +42,34 @@ my %commands = (
 		"00" => sub { print $p[$p[$pos+1]] },
 		"01" => sub { print $p[$pos+1] },
 		"inc" => 2
+	},
+	5 => {
+		"00" => sub { if ($p[$p[$pos+1]] != 0) { $pos = $p[$p[$pos+2]] } else { $pos += 3 } },
+		"01" => sub { if ($p[$pos+1] != 0) { $pos = $p[$p[$pos+2]] } else { $pos += 3 } },
+		"10" => sub { if ($p[$p[$pos+1]] != 0) { $pos = $p[$pos+2] } else { $pos += 3 } },
+		"11" => sub { if ($p[$pos+1] != 0) { $pos = $p[$pos+2] } else { $pos += 3 } },
+		"inc" => 0
+	},
+	6 => {
+		"00" => sub { if ($p[$p[$pos+1]] == 0) { $pos = $p[$p[$pos+2]] } else { $pos += 3 }},
+		"01" => sub { if ($p[$pos+1] == 0) { $pos = $p[$p[$pos+2]] } else { $pos += 3 } },
+		"10" => sub { if ($p[$p[$pos+1]] == 0) { $pos = $p[$pos+2] } else { $pos += 3} },
+		"11" => sub { if ($p[$pos+1] == 0) { $pos = $p[$pos+2] } else { $pos += 3 } },
+		"inc" => 0
+	},
+	7 => {
+		"00" => sub { $p[$p[$pos+3]] = $p[$p[$pos+1]] < $p[$p[$pos+2]] ? 1 : 0 },
+		"01" => sub { $p[$p[$pos+3]] = $p[$pos+1] < $p[$p[$pos+2]] ? 1 : 0 },
+		"10" => sub { $p[$p[$pos+3]] = $p[$p[$pos+1]] < $p[$pos+2] ? 1 : 0 },
+		"11" => sub { $p[$p[$pos+3]] = $p[$pos+1] < $p[$pos+2] ? 1 : 0 },
+		"inc" => 4
+	},
+	8 => {
+		"00" => sub { $p[$p[$pos+3]] = $p[$p[$pos+1]] == $p[$p[$pos+2]] ? 1 : 0 },
+		"01" => sub { $p[$p[$pos+3]] = $p[$pos+1] == $p[$p[$pos+2]] ? 1 : 0 },
+		"10" => sub { $p[$p[$pos+3]] = $p[$p[$pos+1]] == $p[$pos+2] ? 1 : 0 },
+		"11" => sub { $p[$p[$pos+3]] = $p[$pos+1] == $p[$pos+2] ? 1 : 0 },
+		"inc" => 4
 	},
 	99 => {
 		"00" => sub { $exit = 1 }
@@ -67,4 +100,3 @@ chomp(my $line = <$fh>);
 close $fh;
 @p = split(/,/,$line);
 run_program();
-
