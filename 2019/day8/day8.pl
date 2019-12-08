1e9c245432b3447f95aaf88c7280294f493924db
David Bell <david.bell@gmail.com>
2019-12-08 10:13:49 +0000

    part 2 done

--- 2019/day8/day8.pl
+++ 2019/day8/day8.pl
@@ -8,13 +8,26 @@ close $fh;
 
 my @digits = split(//, $line);
 
+my @grid;
+
 my $layer_size = 25 * 6;
 my $zeroes = 99999;
 my $result;
 while(@digits) {
 	my @layer = splice(@digits, 0, $layer_size);
+
+	my $pos = 0;
+	for (my $y = 5; $y >= 0; $y--) {
+		for (my $x = 0; $x < 25; $x++) {
+			if (!defined($grid[$y][$x]) || $grid[$y][$x] == 2) {
+				$grid[$y][$x] = $layer[$pos];
+			}
+			$pos++;
+		}
+	}
+
 	my @digit_count;
-	for my $digit (0..9) {
+	for my $digit (0..2) {
 		$digit_count[$digit]++ foreach grep { $_ == $digit} @layer;
 	}
 	if ($digit_count[0] < $zeroes) {
@@ -23,3 +36,10 @@ while(@digits) {
 	}
 }
 print "$result\n";
+
+for (my $y = 5; $y >= 0; $y--) {
+	for (my $x = 0; $x < 25; $x++) {
+		print $grid[$y][$x] == 1 ? '#' : ' ';
+	}
+	print "\n";
+}	
