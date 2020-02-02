5eb8122e38a91355bed991f2158e02ed021f9be1
David Bell <david.bell@gmail.com>
2020-02-02 00:59:49 +0000

    day19 part1

---
+++ 2015/day19/day19.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @replacements;
+
+open my $fh, '<', 'input.txt';
+while (my $line = <$fh>) {
+	chomp($line);
+	last if $line eq '';
+	my ($val, $repl) = split(' => ', $line);
+	push @replacements, [ $val, $repl ];
+}
+my $molecule = <$fh>;
+close $fh;
+
+my %mo;
+
+LOOP:
+foreach my $r (@replacements) {
+	my ($val, $repl) = @$r;
+
+	my $n = 0;
+
+	while (1) {
+		my $m = $molecule;
+		for (1..$n) {
+			$m =~ s/$val/-/;
+		}
+		my $found = $m =~ s/$val/$repl/;
+		next LOOP unless $found;
+		$m =~ s/-/$val/g;
+		$mo{$m} = 1;
+		$n++;
+	}
+}
+
+print scalar keys %mo;
+print "\n";
