a4d973d654460c088b73f97b5875c13f9dd8a415
David Bell <david.bell@gmail.com>
2020-02-21 16:23:20 +0000

    part2 done

--- 2016/day2/day2.cpp
+++ 2016/day2/day2.cpp
@@ -4,18 +4,12 @@
 #include <vector>
 using namespace std;
 
-#define SIZE 3
+struct Pos {
+	int x;
+	int y;
+};
 
 int main() {
-	int grid[SIZE][SIZE];
-	int n = 1;
-	for (int y = 0; y < SIZE; y++) {
-		for (int x = 0; x < SIZE; x++) {
-			grid[x][y] = n;
-			n++;
-		}
-	}
-
 	vector<string> instructions;
 	string line;
 	ifstream in("input.txt");
@@ -24,27 +18,52 @@ int main() {
 	}
 	in.close();
 
-	int x = 1;
-	int y = 1;
+	vector<vector<string>> grids;
+	vector<Pos> starts;
 
-	for (string& line : instructions) {	
-		for (char& direction : line) {
-			if (direction == 'U' && y > 0) {
-				y--;
+	vector<string> grid;
+	grid.push_back("123");
+	grid.push_back("456");
+	grid.push_back("789");
+	grids.push_back(grid);
+	Pos pos = Pos{ .x = 1, .y = 1 };
+	starts.push_back(pos);
+
+	vector<string> grid2;
+	grid2.push_back("00100");
+	grid2.push_back("02340");
+	grid2.push_back("56789");
+	grid2.push_back("0ABC0");
+	grid2.push_back("00D00");
+	grids.push_back(grid2);
+	Pos pos2 = Pos{ .x = 0, .y = 2 };
+	starts.push_back(pos2);
+
+	for (int i = 0; i < grids.size(); i++) {
+		grid = grids[i];
+		Pos startpos = starts[i];
+		int x = startpos.x;
+		int y = startpos.y;
+
+		for (string& line : instructions) {
+			for (char& direction : line) {
+				if (direction == 'U' && y > 0 && grid[y-1][x] != '0') {
+					y--;
+				}
+				else if (direction == 'D' && y < grid.size() - 1 && grid[y+1][x] != '0') {
+					y++;
+				}
+				else if (direction == 'L' && x > 0 && grid[y][x-1] != '0') {
+					x--;
+				}
+				else if (direction == 'R' && x < grid.size() - 1 && grid[y][x+1] != '0') {
+					x++;
+				}
 			}
-			else if (direction == 'D' && y < SIZE - 1) {
-				y++;
-			}
-			else if (direction == 'L' && x > 0) {
-				x--;
-			}
-			else if (direction == 'R' && x < SIZE - 1) {
-				x++;
-			}
+			cout << grid[y][x];
 		}
-		cout << grid[x][y];
-	}
 
-	cout << endl;
+		cout << endl;
+	}
 	return 0;
 }
