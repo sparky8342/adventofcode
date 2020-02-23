fe8e6da2cbf4f1defc723d101e694bf4da6e9af0
David Bell <david.bell@gmail.com>
2020-02-23 18:44:42 +0000

    part2

--- 2016/day7/day7.cpp
+++ 2016/day7/day7.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
 using namespace std;
 
 int main() {
 	int valid = 0;
+	int ssl = 0;
 	string ip_address;
 	ifstream in("input.txt");
 	while (getline(in, ip_address)) {
-
 		bool in_brackets = false;
 		bool found = false;
+		bool abba_result = false;
+		map <string, bool> aba;
+		map <string, bool> bab;
 		for (int i = 0; i < ip_address.size() - 2; i++) {
 			if (ip_address[i] == '[') {
 				in_brackets = true;
@@ -20,16 +24,26 @@ int main() {
 			}
 			else {
 				if (
-					ip_address[i] != ip_address[i + 1]
+					abba_result == false
+					&& ip_address[i] != ip_address[i + 1]
 					&& ip_address[i] == ip_address[i + 3]
 					&& ip_address[i + 1] == ip_address[i + 2]
 				) {
 					if (in_brackets == true) {
 						found = false;
-						break;
+						abba_result = true;
 					}
 					else {
 						found = true;
+					}
+				}
+				if (ip_address[i] != ip_address[i + 1] && ip_address[i] == ip_address[i + 2]) {
+					string str = ip_address.substr(i, 3);
+					if (in_brackets == true) {
+						bab[str] = true;
+					}
+					else {
+						aba[str] = true;
 					}
 				}
 			}
@@ -37,9 +51,17 @@ int main() {
 		if (found == true) {
 			valid++;
 		}
-
-        }
-        in.close();
-	cout << valid << endl;
+		for(map<string, bool>::iterator iter = aba.begin(); iter != aba.end(); ++iter) {
+			string k = iter->first;
+			string match;
+			match = match + k[1] + k[0] + k[1];
+			if (bab.count(match) == 1) {
+				ssl++;
+				break;
+			}
+		}
+	}
+	in.close();
+	cout << valid << endl << ssl << endl;
 	return 0;
 }
