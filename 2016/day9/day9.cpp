11476f9aa17c9d07d8d514cb0093ab8e8b266f13
David Bell <david.bell@gmail.com>
2020-02-26 00:04:01 +0000

    day9 part2 done

--- 2016/day9/day9.cpp
+++ 2016/day9/day9.cpp
@@ -4,15 +4,10 @@
 #include <regex>
 using namespace std;
 
-int main() {
-	string line;
-	ifstream in("input.txt");
-	getline(in, line);
-	in.close();
-	
-	string new_line;
+long decompress(string line, bool recurse) {
 	const regex reg("(.*?)\\(([0-9]+)x([0-9]+)\\)(.*)");
 	smatch match;
+	long count = 0;
 
 	while (regex_match(line, match, reg)) {
 		string prefix = match[1];
@@ -20,17 +15,34 @@ int main() {
 		int no_repeat = stoi(match[3]);
 		string remaining = match[4];
 
-		new_line = new_line + prefix;
+		count += prefix.size();
 
-		string section;
-		section = remaining.substr(0, no_chars);
-		for (int i = 0; i < no_repeat; i++) {
-			new_line = new_line + section;
-		}		
+		string section = remaining.substr(0, no_chars);
+		if (recurse) {
+			count += decompress(section, true) * no_repeat;
+		}
+		else {
+			count += section.size() * no_repeat;
+		}
 
 		line = remaining.substr(no_chars, remaining.size() - no_chars);
 	}
 
-	cout << new_line.size() << endl;
+	count += line.size();
+	return count;
+}
+
+int main() {
+	string line;
+	ifstream in("input.txt");
+	getline(in, line);
+	in.close();
+
+	long count = decompress(line, false);
+	cout << count << endl;
+
+	count = decompress(line, true);
+	cout << count << endl;
+
 	return 0;
 }
