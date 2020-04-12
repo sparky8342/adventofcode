c28215c71fd56d0f00f122e7471b1abccd1d1785
David Bell <david.bell@gmail.com>
2020-04-12 03:57:29 +0100

    day19 part2

--- 2016/day19/day19.cpp
+++ 2016/day19/day19.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 #define NUM_ELVES 3005290
 
-int main() {
+list<int> init_elves() {
 	int num_elves = NUM_ELVES;
 
 	// use a double linked list
@@ -14,9 +14,14 @@ int main() {
 	for (int i = 0; i < num_elves; i++) {
 		elves.push_back(num_elves - i);
 	}
+	return elves;
+}
+
+int part1() {
+	list<int> elves = init_elves();
 
 	list<int>::iterator it;
-       	it = elves.end();
+	it = elves.end();
 	it--;
 
 	while (elves.size() > 1) {
@@ -37,7 +42,58 @@ int main() {
 		it = newit;
 	}
 
-	cout << *elves.begin() << endl;
+	return *elves.begin();
+}
+
+int part2() {
+	list<int> elves = init_elves();
+
+	list<int>::iterator it;
+	it = elves.end();
+	it--;
+
+	list<int>::iterator oppositeit = it;
+	int steps = elves.size() / 2;
+	for (int i = 1; i <= steps; i++) {
+		oppositeit--;
+	}
+
+	while (elves.size() > 1) {
+		list<int>::iterator eraseit = oppositeit;
+		int moves = 1;
+		if (elves.size() % 2 == 1) {
+			moves++;
+		}
+		for (int i = 1; i <= moves; i++) {
+			if (oppositeit == elves.begin()) {
+				oppositeit = elves.end();
+			}
+			oppositeit--;
+		}
+		elves.erase(eraseit);
+
+		if (it == elves.begin()) {
+			it = elves.end();
+		}
+		it--;
+
+		if (it == oppositeit) {
+			if (oppositeit == elves.begin()) {
+				oppositeit = elves.end();
+			}
+			oppositeit--;
+		}
+	}
+
+	return *elves.begin();
+}
+
+
+int main() {
+	int elf = part1();
+	cout << elf << endl;
+	elf = part2();
+	cout << elf << endl;
 
 	return 0;
 }
