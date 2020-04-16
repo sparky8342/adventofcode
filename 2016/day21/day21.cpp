7833c5909f1cdddbc83e4cb550d609d2bc2fc5cb
David Bell <david.bell@gmail.com>
2020-04-16 16:06:23 +0100

    Rearrange to make part2 easier

--- 2016/day21/day21.cpp
+++ 2016/day21/day21.cpp
@@ -3,7 +3,7 @@
 #include <regex>
 using namespace std;
 
-int main() {
+vector<char> scramble(vector<char>code, vector<string> &operations) {
 	const regex swap_pos("^swap position ([0-9]+) with position ([0-9]+)$");
 	const regex swap_letter("^swap letter ([a-z]) with letter ([a-z])$");
 	const regex rotate_left("^rotate left ([0-9]+) step(s)?$");
@@ -12,20 +12,17 @@ int main() {
 	const regex reverse_pos("^reverse positions ([0-9]+) through ([0-9]+)$");
 	const regex move("^move position ([0-9]+) to position ([0-9]+)$");
 
-	vector<char> code = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-
-	string line;
-	ifstream in("input.txt");
-	while (getline(in, line)) {
+	vector<string>::iterator it;
+	for (it=operations.begin(); it!=operations.end(); ++it) {
 		smatch match;
-		if (regex_match(line, match, swap_pos)) {
+		if (regex_match(*it, match, swap_pos)) {
 			int x = stoi(match[1]);
 			int y = stoi(match[2]);
 			char tmp = code[x];
 			code[x] = code[y];
 			code[y] = tmp;
 		}
-		else if (regex_match(line, match, swap_letter)) {
+		else if (regex_match(*it, match, swap_letter)) {
 			int x;
 			int y;
 			for (int i = 0; i < code.size(); i++) {
@@ -40,15 +37,15 @@ int main() {
 			code[x] = code[y];
 			code[y] = tmp;
 		}
-		else if (regex_match(line, match, rotate_left)) {
+		else if (regex_match(*it, match, rotate_left)) {
 			int x = stoi(match[1]);
 			rotate(code.begin(), code.begin() + x, code.end()); 
 		}
-		else if (regex_match(line, match, rotate_right)) {
+		else if (regex_match(*it, match, rotate_right)) {
 			int x = stoi(match[1]);
 			rotate(code.begin(), code.begin() + code.size() - x, code.end()); 
 		}
-		else if (regex_match(line, match, rotate_pos)) {
+		else if (regex_match(*it, match, rotate_pos)) {
 			int x;
 			for (int i = 0; i < code.size(); i++) {
 				if (code[i] == match[1]) {
@@ -63,7 +60,7 @@ int main() {
 			x = x % code.size();
 			rotate(code.begin(), code.begin() + code.size() - x, code.end());
 		}
-		else if (regex_match(line, match, reverse_pos)) {
+		else if (regex_match(*it, match, reverse_pos)) {
 			int x = stoi(match[1]);
 			int y = stoi(match[2]);
 			vector<char> new_code = code;
@@ -75,17 +72,29 @@ int main() {
 			}
 			code = new_code;
 		}
-		else if (regex_match(line, match, move)) {
+		else if (regex_match(*it, match, move)) {
 			int x = stoi(match[1]);
 			int y = stoi(match[2]);
 			int val = code[x];
 			code.erase(code.begin() + x);
 			code.insert(code.begin() + y, val);
 		}
-	
 	}
 
+	return code;
+}
+
+int main() {
+	vector<string> operations;
+	string line;
+	ifstream in("input.txt");
+	while (getline(in, line)) {
+		operations.push_back(line);
+	}
 	in.close();
+
+	vector<char> code = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+	code = scramble(code, operations);
 
 	for (int i = 0; i < code.size(); i++) {
 		cout << code[i];
