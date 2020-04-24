7a2c906a8fbd4cf7319f7e20720a4acfd3080e37
David Bell <david.bell@gmail.com>
2020-04-24 14:59:01 +0100

    variable rename

--- 2017/day1/day1.lisp
+++ 2017/day1/day1.lisp
@@ -4,22 +4,22 @@
 
 (let* ((in (uiop:read-file-string "input.txt"))
 	; coerce input into a list, removing final carriage return
-	(digits (butlast (coerce in 'list)))
-	(len (length digits))
+	(captcha (butlast (coerce in 'list)))
+	(len (length captcha))
 	(half_len (/ len 2))
 	(sum_part1 0)
 	(sum_part2 0))
 
 	; repeat the list
-	(setf digits (append digits digits))
+	(setf captcha (append captcha captcha))
 
 	(loop for i from 0 to (- len 1) do
 		; part 1
-		(if (eq (nth i digits) (nth (+ i 1) digits))
-			(setq sum_part1 (+ sum_part1 (digit-char-p (nth i digits)))))
+		(if (eq (nth i captcha) (nth (+ i 1) captcha))
+			(setq sum_part1 (+ sum_part1 (digit-char-p (nth i captcha)))))
 		; part 2
-		(if (eq (nth i digits) (nth (+ i half_len) digits))
-			(setq sum_part2 (+ sum_part2 (digit-char-p (nth i digits))))))
+		(if (eq (nth i captcha) (nth (+ i half_len) captcha))
+			(setq sum_part2 (+ sum_part2 (digit-char-p (nth i captcha))))))
 
 	(princ sum_part1)
 	(terpri)
