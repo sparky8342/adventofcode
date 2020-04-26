d25d66d20228160b6c13001d6b3078658ca6c783
David Bell <david.bell@gmail.com>
2020-04-26 02:44:18 +0100

    part2 done

--- 2017/day2/day2.lisp
+++ 2017/day2/day2.lisp
@@ -8,13 +8,26 @@
 (defun min-max-difference (vals)
 	(- (apply 'max vals) (apply 'min vals)))
 
-(let ((checksum 0)
+(defun division (vals)
+	(loop named loop-1
+		for n in vals
+		do (loop for n2 in vals
+			do (if (> n n2)
+				(let ((div (/ n n2)))
+					(if (= div (floor div))
+						(return-from loop-1 (values div))))))))
+
+(let ((part1 0)
+	(part2 0)
 	(in (open "input.txt" :if-does-not-exist nil)))
 	(when in
 		(loop for line = (read-line in nil)
 			while line do
 				(let ((vals (convert-line line)))
-				(setq checksum (+ checksum (min-max-difference vals)))))
+					(setq part1 (+ part1 (min-max-difference vals)))
+					(setq part2 (+ part2 (division vals))))))
 	(close in)
-	(princ checksum)
-	(terpri)))
+	(princ part1)
+	(terpri)
+	(princ part2)
+	(terpri))
