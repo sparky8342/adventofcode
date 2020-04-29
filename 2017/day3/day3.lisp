0f6dc51eb45a7a0f147d4c23bc053097ca4da940
David Bell <david.bell@gmail.com>
2020-04-29 20:20:33 +0100

    day3 part2 done

--- 2017/day3/day3.lisp
+++ 2017/day3/day3.lisp
@@ -12,6 +12,39 @@
 		(let ((d (list (- x n) (- n (- x side)))))
 			(- side (apply 'min d)))))
 
+(defun get-grid-key (x y)
+	(concatenate 'string (write-to-string x) ":" (write-to-string y)))
+
+(defun surround-total (grid x y)
+	(let ((sum 0))
+		(loop for dx in '(-1 0 1) do
+			(loop for dy in '(-1 0 1) do
+				(let ((key (get-grid-key (+ x dx) (+ y dy))))
+					(if (gethash key grid)
+						(setq sum (+ sum (gethash key grid)))))))
+		(+ 0 sum)))
+
+(defun spiral (n)
+	(let ((grid (make-hash-table :test #'equal))
+		(x 0)
+		(y 0)
+		(key (get-grid-key 0 0)))
+
+		(setf (gethash key grid) 1)
+		(loop while (> n (gethash key grid)) do
+			(if (and (or (/= x y) (>= x 0)) (<= (abs x) (abs y)))
+				(if (>= y 0)
+					(setq x (+ x 1))
+					(setq x (- x 1)))
+				(if (>= x 0)
+					(setq y (- y 1))
+					(setq y (+ y 1))))
+			(setq key (get-grid-key x y))
+			(setf (gethash key grid) (surround-total grid x y)))
+		(gethash key grid)))
+
 (let ((input 361527))
 	(princ (distance input))
+	(terpri)
+	(princ (spiral input))
 	(terpri))
