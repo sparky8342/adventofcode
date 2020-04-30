f750ae195c4231a32f92fcb0c8fc0199b6d923ad
David Bell <david.bell@gmail.com>
2020-04-30 12:00:47 +0100

    day4 part2

--- 2017/day4/day4.lisp
+++ 2017/day4/day4.lisp
@@ -3,25 +3,38 @@
 (require "asdf")
 
 (defun convert-line (line)
-	(map 'list (lambda (x) (read-from-string x)) (uiop:split-string line :separator "  ")))
+	(map 'list (lambda (x) (read-from-string x)) (uiop:split-string line :separator " ")))
 
-(defun all-unique (l)
-	(if l
-		(if (member (car l) (cdr l))
+(defun convert-line-sort-chars (line)
+	(map 'list (lambda (x) (sort-chars x)) (uiop:split-string line :separator " ")))
+
+(defun sort-chars (str)
+	(read-from-string (format nil "~{~a~}" (sort (coerce str 'list) #'char-lessp))))
+
+(defun all-unique (lst)
+	(if lst
+		(if (member (car lst) (cdr lst))
 			nil
-			(all-unique (cdr l)))
+			(all-unique (cdr lst)))
 		t))
 
 (defun count-valid (file)
-	(let ((valid 0)
+	(let ((part1 0)
+		(part2 0)
 		(in (open file :if-does-not-exist nil)))
 			(when in
 				(loop for line = (read-line in nil)
 					while line do
-						(let ((vals (convert-line line)))
+						(let ((vals (convert-line line))
+							(vals2 (convert-line-sort-chars line)))
 							(if (all-unique vals)
-								(setq valid (+ valid 1))))))
-		valid))
+								(setq part1 (+ part1 1)))
+							(if (all-unique vals2)
+								(setq part2 (+ part2 1))))))
+		(list part1 part2)))
 
-(princ (count-valid "input.txt"))
-(terpri)
+(let ((ans (count-valid "input.txt")))
+	(princ (car ans))
+	(terpri)
+	(princ (car (cdr ans)))
+	(terpri))
