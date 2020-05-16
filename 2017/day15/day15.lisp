c792c15990a49a771ce853ce8b136a6a448a7673
David Bell <david.bell@gmail.com>
2020-05-16 03:51:48 +0100

    day15 part2

--- 2017/day15/day15.lisp
+++ 2017/day15/day15.lisp
@@ -11,16 +11,33 @@
 (defun next-b (n)
 	(next 48271 n))
 
+(defun next-a-2 (n)
+	(setq n (next-a n))
+	(loop while (> (mod n 4) 0) do
+		(setq n (next-a n)))
+	n)
+
+(defun next-b-2 (n)
+	(setq n (next-b n))
+	(loop while (> (mod n 8) 0) do
+		(setq n (next-b n)))
+	n)
+
 (defun bot16 (n)
 	(mod n 65536))
 
-(let ((a 277)
-	(b 349)
-	(matches 0))
-		(dotimes (i 40000000)
-			(setq a (next-a a))
-			(setq b (next-b b))
+(defun count-matches (a b gen-a gen-b amount)
+	(let ((matches 0))
+		(loop repeat amount do
+			(setq a (funcall gen-a a))
+			(setq b (funcall gen-b b))
 			(if (= (bot16 a) (bot16 b))
 				(setq matches (+ matches 1))))
-		(princ matches)
+		matches))
+
+(let ((a 277)
+	(b 349))
+		(princ (count-matches a b (lambda (x) (next-a x)) (lambda (x) (next-b x)) 40000000))
+		(terpri)
+		(princ (count-matches a b (lambda (x) (next-a-2 x)) (lambda (x) (next-b-2 x)) 5000000))
 		(terpri))
