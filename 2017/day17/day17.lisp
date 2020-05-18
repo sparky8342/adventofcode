94695a6cee73c58926c39eec35a7b6ae1828143f
David Bell <david.bell@gmail.com>
2020-05-18 02:21:26 +0100

    day17 part2 done

--- 2017/day17/day17.lisp
+++ 2017/day17/day17.lisp
@@ -2,7 +2,9 @@
 
 (let ((steps 303)
 	(lst '(0))
-	(pos 0))
+	(pos 0)
+	(pos1 0))
+		; part 1 (insert values, find value after 2017)
 		(loop for x from 1 to 2017 do
 			(setq pos (mod (+ pos steps) x))
 			(push x (cdr (nthcdr pos lst)))
@@ -11,4 +13,13 @@
 		(if (= pos 2019)
 			(setq pos 0))
 		(princ (nth pos lst))
+		(terpri)
+
+		; part 2 (don't insert, just save the value when at position 1)
+		(loop for x from 1 to 50000000 do
+			(setq pos (mod (+ pos steps) x))
+			(setq pos (+ pos 1))
+			(if (= pos 1)
+				(setq pos1 x)))
+		(princ pos1)
 		(terpri))
