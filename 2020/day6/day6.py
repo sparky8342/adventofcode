4894c0c1815ddb074cc88850ab7d050a2433ed7a
davidbell <david.bell@gmail.com>
2020-12-06 12:31:34 +0000

    Reworked, more readable

--- 2020/day6/day6.py
+++ 2020/day6/day6.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 import functools
+import sys
 
 part1 = 0
 part2 = 0
-with open('input.txt') as f:
-	questions = set()
-	question_sets = []
-	for line in f:
-		line = line.strip()
 
-		if line:
-			question_set = set()
-			for char in line:
-				questions.add(char)
-				question_set.add(char)
-			question_sets.append(question_set)
-		else:
-			part1 += len(questions)
-			questions = set()
-			reduced = functools.reduce(lambda a,b : a.intersection(b), question_sets)
-			part2 += len(reduced)
-			question_sets = []
+data = open('input.txt').read().strip()
+groups = data.split("\n\n")
+
+for group in groups:
+	people = group.split("\n")
+
+	questions = set()
+	for person in people:
+		for q in person:
+			questions.add(q)
 
 	part1 += len(questions)
+
+	question_sets = []
+	for person in people:
+		question_set = set()
+		for q in person:
+			question_set.add(q)
+		question_sets.append(question_set)
+
 	reduced = functools.reduce(lambda a,b : a.intersection(b), question_sets)
-	part2 += len(reduced)	
+	part2 += len(reduced)
 
 print(part1)
 print(part2)
