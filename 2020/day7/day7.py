ab791b3fb2f14279a27d64e96d05609d0d639821
davidbell <david.bell@gmail.com>
2020-12-07 23:57:57 +0000

    changed to use objects

--- 2020/day7/day7.py
+++ 2020/day7/day7.py
@@ -2,12 +2,23 @@
 import re
 import functools
 
+class Node:
+	def __init__(self, name):
+		self.name = name
+		self.parents = []
+		self.children = []
+
+class ChildLink:
+	def __init__(self, child, amount):
+		self.child = child
+		self.amount = amount
+
 # walk up tree to find all parents
 @functools.lru_cache(maxsize=None) # memoize this function
 def find_parents(node):
 	parents = set()
-	for parent in nodes[node]['parents']:
-		parents.add(parent)
+	for parent in node.parents:
+		parents.add(parent.name)
 		parents = parents.union(find_parents(parent))
 	return parents
 
@@ -15,14 +26,14 @@ def find_parents(node):
 @functools.lru_cache(maxsize=None)
 def count_bags(node):
 	total = 1
-	for child in nodes[node]['children']:
-		total += child['amount'] * count_bags(child['name'])
+	for child_link in node.children:
+		total += child_link.amount * count_bags(child_link.child)
 	return total	
 
-def get_node(node):
-	if node not in nodes:
-		nodes[node] = { 'parents' : [], 'children' : [] }
-	return nodes[node]
+def get_node(name):
+	if name not in nodes:
+		nodes[name] = Node(name)
+	return nodes[name]
 
 nodes = {}
 data = open('input.txt').read().splitlines()
@@ -41,10 +52,12 @@ for line in data:
 		for child in children:
 			amount = int(child[0])
 			child_name = child[2:]
-			node['children'].append({ 'name' : child_name, 'amount' : amount })
-			get_node(child_name)['parents'].append(name)
+			child_node = get_node(child_name)
+			node.children.append(ChildLink(child_node, amount))
+			child_node.parents.append(node)
 	
 	nodes[name] = node
 
-print(len(find_parents('shiny gold')))
-print(count_bags('shiny gold') - 1)
+shiny_gold = nodes['shiny gold']
+print(len(find_parents(shiny_gold)))
+print(count_bags(shiny_gold) - 1)
