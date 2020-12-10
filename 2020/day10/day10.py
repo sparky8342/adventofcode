dc91a696b95b5a87c36b8ad7e3445768a8760649
David Bell <david.bell@gmail.com>
2020-12-10 12:20:23 +0000

    day10 part2 done

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
+import functools
+
+@functools.lru_cache(maxsize=None)
+def search(num):
+	total = 0
+	end = True
+	for i in (num + 1, num + 2, num + 3):
+		if i in data_set:
+			end = False
+			total += search(i)
+
+	if end:
+		total += 1
+
+	return total
 
 data = sorted([int(i) for i in open('input.txt').read().splitlines()])
 data.insert(0, 0)
 data.append(data[len(data) - 1] + 3)
 
+# part1
 diff_one = 0
 diff_three = 0
 
@@ -14,3 +30,7 @@ for i in range(0, len(data) - 1):
 		diff_three += 1
 
 print(diff_one * diff_three)
+
+# part 2
+data_set = set(i for i in data)
+print(search(0))
