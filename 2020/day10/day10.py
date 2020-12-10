af6dc89790de8fe4ca69cde3ac370fcb1386aa53
David Bell <david.bell@gmail.com>
2020-12-10 12:47:44 +0000

    use the set for part1 and 2

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -6,7 +6,7 @@ def search(num):
 	total = 0
 	end = True
 	for i in (num + 1, num + 2, num + 3):
-		if i in data_set:
+		if i in numbers:
 			end = False
 			total += search(i)
 
@@ -15,22 +15,25 @@ def search(num):
 
 	return total
 
-data = sorted([int(i) for i in open('input.txt').read().splitlines()])
-data.insert(0, 0)
-data.append(data[len(data) - 1] + 3)
+numbers = set([int(i) for i in open('input.txt').read().splitlines()])
+numbers.add(0)
+numbers.add(max(numbers) + 3)
 
 # part1
 diff_one = 0
 diff_three = 0
-
-for i in range(0, len(data) - 1):
-	if data[i + 1] - data[i] == 1:
+n = 0
+while(1):
+	if n + 1 in numbers:
 		diff_one += 1
-	elif data[i + 1] - data[i] == 3:
+		n += 1
+	elif n + 3 in numbers:
 		diff_three += 1
+		n += 3
+	else:
+		break
 
 print(diff_one * diff_three)
 
 # part 2
-data_set = set(i for i in data)
 print(search(0))
