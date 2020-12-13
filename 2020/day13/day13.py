117caec06df3f89b220b41ce0e11e11019689d8c
davidbell <david.bell@gmail.com>
2020-12-13 18:07:46 +0000

    day13 part2 done

--- 2020/day13/day13.py
+++ 2020/day13/day13.py
@@ -2,12 +2,40 @@
 
 with open('input.txt') as f:
 	leave_time = int(f.readline().strip())
-	buses = [int(x) for x in f.readline().strip().split(",") if x != 'x']
+	buses = [x for x in f.readline().strip().split(",")]
 
+# part 1
 times = []
 for bus in buses:
+	if bus == 'x':
+		continue
+	bus = int(bus)
 	times.append((bus - leave_time % bus, bus))
 
 times = sorted(times)
 time = times[0]
 print(time[0] * time[1])
+
+# part 2
+targets = []
+positions = []
+nums = []
+for i, bus in enumerate(buses):
+	if bus == 'x':
+		continue
+	targets.append((int(bus), i))
+	positions.append([-i, int(bus)])
+	nums.append(int(bus))
+
+num = targets.pop(0)
+inc = num[0]
+position = num[1] * -1
+
+for target in targets:
+	time, offset = target
+	while (position + offset) % time != 0:
+		position += inc
+
+	inc *= time
+
+print(position)
