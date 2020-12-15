a72003eae47e675741ac0e8c2b013138f60cb0bc
davidbell <david.bell@gmail.com>
2020-12-15 19:22:50 +0000

    range is quicker

--- 2020/day15/day15.py
+++ 2020/day15/day15.py
@@ -5,16 +5,14 @@ def get(sequence, target):
 	for i, n in enumerate(sequence[:-1]):
 		seen[n] = i
 	last = sequence[-1]
-	pos = len(sequence) - 1
 
-	while pos < target - 1:
+	for pos in range(len(sequence) - 1, target - 1):
 		nxt = 0
 		if last in seen:
 			nxt = pos - seen[last]
 
 		seen[last] = pos
 		last = nxt
-		pos += 1
 
 	return last
 
