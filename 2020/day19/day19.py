8830d77df39aba5e40d77eeab4b276810acde3e6
davidbell <david.bell@gmail.com>
2020-12-19 13:05:09 +0000

    day19 part2 done

--- 2020/day19/day19.py
+++ 2020/day19/day19.py
@@ -11,9 +11,7 @@ def eval_rules(rule_no):
 	rule = rules[rule_no]
 	tokens = rule.split()
 	for token in tokens:
-		if token == '|':
-			rule_text = rule_text + '|'
-		elif token.isnumeric():
+		if token.isnumeric():
 			rule_text = rule_text + eval_rules(token) 
 		else:
 			rule_text = rule_text + token
@@ -45,9 +43,29 @@ with open('input.txt') as f:
 		messages.append(line)
 		line = f.readline().strip()
 
+# part 1
 regex = eval_rules('0')
 regex = '^' + regex + '$'
 
+valid = 0
+for message in messages:
+	if re.match(regex, message):
+		valid += 1
+
+print(valid)
+
+# part 2
+# these rules from the puzzle:
+#rules['8'] = '42 | 42 8'
+#rules['11'] = '42 31 | 42 11 31'
+
+# converted by hand:
+rules['8'] = '42 {1,}'
+rules['11'] = '42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31'
+
+cache = {}
+regex = eval_rules('0')
+regex = '^' + regex + '$'
 valid = 0
 for message in messages:
 	if re.match(regex, message):
