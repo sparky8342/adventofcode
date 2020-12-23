2fffa471e4adf235eb367b1ff1f141e877b8a3ec
davidbell <david.bell@gmail.com>
2020-12-23 17:36:04 +0000

    day23 part2 done - custom circular list implementation

--- 2020/day23/day23.py
+++ 2020/day23/day23.py
@@ -1,24 +1,80 @@
 #!/usr/bin/python3
-from collections import deque
 
-def play_game(cups, turns, part):
+class CircularList:
+	# circular list - each element is [value, prev_pos, next_pos]
+	# lookup hash used to avoid scanning through for values
+
+	def __init__(self):
+		self.lst = []
+		self.lookup = {}
+		self.deleted = []
+
+	def __str__(self):
+		st = ''
+		for item in self.lst:
+			st += ",".join(str(x) for x in item) + ' '
+		return st
+
+	def append_to_list(self, value):
+		if len(self.lst) == 0:
+			self.lst.append([value, 0, 0])
+			self.lookup[value] = 0
+		else:
+			new = [value, len(self.lst) - 1, 0]
+			self.lst[-1][2] = len(self.lst)
+			self.lst.append(new)
+			self.lst[0][1] = len(self.lst) - 1
+			self.lookup[value] = len(self.lst) - 1
+
+	def get(self, position):
+		return self.lst[position][0]
+
+	def del_three_after(self, position):
+		removed = []
+		entry = self.lst[position]
+		nxt = entry[2]
+		for _ in range(3):
+			removed.append(self.lst[nxt][0])
+			self.lst[nxt][0] = -1
+			self.deleted.append(nxt)
+			nxt = self.lst[nxt][2]
+
+		self.lst[position][2] = nxt
+		self.lst[nxt][1] = position
+		return removed
+
+	def add_after_pos(self, pos, value):
+		# assuming we always have deleted positions,
+		# which is true for this puzzle
+		free_pos = self.deleted.pop()
+		self.lst[free_pos] = [value, pos, self.lst[pos][2]]
+		self.lst[pos][2] = free_pos
+		self.lookup[value] = free_pos
+		return free_pos
+
+	def add_after_value(self, value, to_add):
+		pos = self.lookup[value]
+		for val in to_add:
+			pos = self.add_after_pos(pos, val)
+
+	def next(self, pos):
+		nxt = self.lst[pos][2]
+		return self.lst[nxt][0], nxt
+
+	def elements_after_value(self, value, amount):
+		pos = self.lookup[value]
+		out = []
+		for _ in range(amount):
+			pos = self.lst[pos][2]
+			out.append(self.lst[pos][0])
+		return out
+
+def play_game(cups, length, turns, part):
 	cup_pos = 0
-	cup = cups[cup_pos]
+	cup = cups.get(cup_pos)
 
-	length = len(cups)
-
-	removed = [0, 0, 0]
-
-	for turn in range(turns):
-		remove_pos = cup_pos + 1
-		for i in range(3):
-			if remove_pos == length:
-				remove_pos = 0
-			removed[i] = cups[remove_pos]
-			remove_pos += 1
-
-		for n in removed:
-			cups.remove(n)
+	for _ in range(turns):
+		removed = cups.del_three_after(cup_pos)
 
 		destination_cup = cup - 1
 		while True:
@@ -28,33 +84,31 @@ def play_game(cups, turns, part):
 				break
 			destination_cup -= 1
 
-		dest_pos = cups.index(destination_cup)
-		dest_pos += 1
-		for n in removed:
-			cups.insert(dest_pos, n)
-			dest_pos += 1
+		cups.add_after_value(destination_cup, removed)
+		cup, cup_pos = cups.next(cup_pos)
 
-		cup_pos = (cups.index(cup) + 1) % length
-		cup = cups[cup_pos]
-
-	cups.rotate(cups.index(1) * -1)
 	if part == 1:
-		return "".join(str(x) for x in list(cups)[1:])
+		ans = "".join(str(x) for x in cups.elements_after_value(1, length - 1))
+		return(ans)
 	elif part == 2:
-		print(cups[0], cups[1], cups[2], cups[3])
-		return cups[1] * cups[2]
+		values = cups.elements_after_value(1, 2)
+		return values[0] * values[1]
+
+
+nums = [int(x) for x in open('input.txt').read().strip()]
 
 # part 1
-nums = [int(x) for x in open('input_test.txt').read().strip()]
-cups = deque(nums)
-print(play_game(cups, 100, 1))
+cups = CircularList()
+for n in nums:
+	cups.append_to_list(n)
+
+print(play_game(cups, 9, 100, 1))
 
 # part 2
-cups = deque(nums)
-for i in range (10, 1000000 + 1):
-	cups.append(i)
+cups = CircularList()
+for n in nums:
+	cups.append_to_list(n)
+for i in range (10, 1000001):
+	cups.append_to_list(i)
 
-print(play_game(cups, 10000000, 2))
-
-# 110 too low
-
+print(play_game(cups, 1000000, 10000000, 2))
