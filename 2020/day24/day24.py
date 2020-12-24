f3e6c2ed3bb96f1e717a70ba3e8873cd6caa8715
davidbell <david.bell@gmail.com>
2020-12-24 12:12:04 +0000

    day24 part2 done

--- 2020/day24/day24.py
+++ 2020/day24/day24.py
@@ -3,8 +3,38 @@ import re
 
 lines = open('input.txt').read().splitlines()
 
+def get_neighbours(tile):
+	neighbours = 0
+	spaces = set()
+	for d in [(0, -1, 1), (-1, 0, 1), (1, 0 ,-1), (0, 1, -1), (1, -1, 0), (-1, 1, 0)]:
+		space = (tile[0] + d[0], tile[1] + d[1], tile[2] + d[2])
+		if space in tiles:
+			neighbours += 1
+		else:
+			spaces.add(space)
+
+	return neighbours, spaces
+
+def generation(tiles):
+	new_tiles = set()
+	all_spaces = set()
+
+	for tile in tiles:
+		neighbours, spaces = get_neighbours(tile)
+		if neighbours == 1 or neighbours == 2:
+			new_tiles.add(tile)
+		all_spaces = all_spaces.union(spaces)
+
+	for space in all_spaces:
+		neighbours, _ = get_neighbours(space)
+		if neighbours == 2:
+			new_tiles.add(space)
+
+	return new_tiles
+
 tiles = set()
 
+# part 1
 for line in lines:
 	dirs = re.findall("(se|sw|ne|nw|e|w)", line)
 
@@ -37,5 +67,11 @@ for line in lines:
 		tiles.remove(place)
 	else:
 		tiles.add(place)	
+
+print(len(tiles))
+
+# part 2
+for _ in range(100):
+	tiles = generation(tiles)
 
 print(len(tiles))
