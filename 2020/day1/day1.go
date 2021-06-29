817a8559f5917b2119352a1d85a9637664574ea6
davidbell <david.bell@gmail.com>
2021-06-29 17:52:26 +0100

    day1 go version

---
+++ 2020/day1/day1.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+	"strconv"
+	"sort"
+	"fmt"
+)
+
+func sum2(numbers []int, target int) (int, int) {
+	i := 0
+	j := len(numbers) - 1
+	k := numbers[i] + numbers[j]
+
+	for k != target {
+		if k > target {
+			j--
+		} else {
+			i++
+		}
+		k = numbers[i] + numbers[j]
+	}
+
+	return numbers[i], numbers[j]
+}
+
+func sum3(numbers []int, target int) (int, int, int) {
+	for _, n := range numbers {
+		num1, num2 := sum2(numbers, target - n)
+		if num1 != n && num2 != n {
+			return n, num1, num2
+		}
+	}
+	return 0, 0, 0
+}
+
+func get_data() []int {
+	data, _ := ioutil.ReadFile("input.txt")
+	data = data[:len(data)-1]
+	lines := strings.Split(string(data), "\n")
+	numbers := []int{}
+	for _, line := range lines {
+		num, _ := strconv.Atoi(line)
+		numbers = append(numbers, num)
+	}
+	sort.Ints(numbers)
+	return numbers
+}
+
+func main() {
+	numbers := get_data()
+	target := 2020
+
+	num1, num2 := sum2(numbers, target)
+	fmt.Println(num1 * num2)
+
+	num1, num2, num3 := sum3(numbers, target)
+	fmt.Println(num1 * num2 * num3)
+}
