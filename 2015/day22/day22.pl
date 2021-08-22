7fd6ace848c1dd0d1017379f137f1251f0a5c856
David Bell <david.bell@gmail.com>
2021-08-22 19:40:34 +0100

    re-written, still gives same answer for part 2 though

--- 2015/day22/day22.pl
+++ 2015/day22/day22.pl
@@ -2,132 +2,175 @@
 use strict;
 use warnings;
 
-use Storable qw(dclone);
+use constant MAGIC_MISSILE_COST => 53;
+use constant MAGIC_MISSILE_DAMAGE => 4;
 
-use constant SPELLS => [
-	{
-		name => 'magic missile',
-		cost => 53,
-		damage => 4,
-		heal => 0
-	},
-	{
-		name => 'drain',
-		cost => 73,
-		damage => 2,
-		heal => 2
-	},
-	{
-		name => 'shield',
-		cost => 113,
-		damage => 0,
-		heal => 0,
-		effect => { time => 6, action => { armour => 7 }}
-	},
-	{
-		name => 'poison',
-		cost => 173,
-		damage => 0,
-		heal => 0,
-		effect => { time => 6, action => { damage => 3 }}
-	},
-	{
-		name => 'recharge',
-		cost => 229,
-		damage => 0,
-		heal => 0,
-		effect => { time => 5, action => { mana => 101 }}
-	}
-];
+use constant DRAIN_COST => 73;
+use constant DRAIN_DAMAGE => 2;
+use constant DRAIN_HEAL => 2;
 
-use constant MIN_MANA => 53; # cheapest spell (magic missile)
+use constant SHIELD_COST => 113;
+use constant SHIELD_TIME => 6;
+use constant SHIELD_ARMOUR => 7;
 
-my $start_enemy = {};
+use constant POISON_COST => 173;
+use constant POISON_TIME => 6;
+use constant POISON_DAMAGE => 3;
+
+use constant RECHARGE_COST => 229;
+use constant RECHARGE_TIME => 5;
+use constant RECHARGE_MANA => 101;
+
 open my $fh, '<', 'input.txt';
-foreach my $key ('hit_points', 'damage') {
-	chomp(my $line = <$fh>);
-	$line =~ /(\d+)/;
-	$start_enemy->{$key} = $1;
-}
+my $enemy_hp = <$fh>;
+my $enemy_damage = <$fh>;
 close $fh;
+($enemy_hp) = $enemy_hp =~ /(\d+)/;
+($enemy_damage) = $enemy_damage =~ /(\d+)/;
 
-my $start_player = { hit_points => 50, mana => 500, armour => 0, spent => 0, effects => [] };
+my $mana = bfs($enemy_hp, $enemy_damage, 0);
+print "$mana\n";
 
-my $min_mana = 9999;
+$mana = bfs($enemy_hp, $enemy_damage, 1);
+print "$mana\n";
 
-fight($start_player, $start_enemy);
+sub process_effects {
+	my ($state) = @_;
 
-print "$min_mana\n";
+	if ($state->{poison} > 0) {
+		$state->{enemy_hp} -= POISON_DAMAGE;
+		$state->{poison}--;
+	}
+	if ($state->{recharge} > 0) {
+		$state->{mana} += RECHARGE_MANA;
+		$state->{recharge}--;
+	}
+}
 
-sub fight {
-	my ($pl, $en) = @_;
+sub bfs {
+	my ($enemy_hp, $enemy_damage, $hard_mode) = @_;
 
-	if ($pl->{mana} < MIN_MANA) {
-		return;
+	my $smallest_mana_used = 9999;
+	
+	my $start = {
+		player_turn => 1,
+		hp => 50,
+		mana => 500,
+		enemy_hp => $enemy_hp,
+		shield => 0,
+		poison => 0,
+		recharge => 0,
+		mana_used => 0
+	};
+
+	my @queue = ($start);
+
+	while (@queue > 0) {
+		my $state = shift @queue;
+
+		if ($state->{mana_used} > $smallest_mana_used) {
+			next;
+		}
+
+		if ($state->{player_turn} == 1) {
+
+			my $hp = $state->{hp};
+			if ($hard_mode) {
+				$hp--;
+				if ($hp <= 0) {
+					next;
+				}
+			}
+
+			foreach my $spell ('magic_missile', 'drain', 'shield', 'poison', 'recharge') {
+				my $new_state = {
+					player_turn => 0,
+					hp => $hp,
+					mana => $state->{mana},
+					enemy_hp => $state->{enemy_hp},
+					shield => $state->{shield},
+					poison => $state->{poison},
+					recharge => $state->{recharge},
+					mana_used => $state->{mana_used}
+				};
+
+				process_effects($new_state);
+
+				if ($spell eq 'magic_missile' && $new_state->{mana} >= MAGIC_MISSILE_COST) {
+					$new_state->{mana} -= MAGIC_MISSILE_COST;
+					$new_state->{mana_used} += MAGIC_MISSILE_COST;
+					$new_state->{enemy_hp} -= MAGIC_MISSILE_DAMAGE;
+				}
+				elsif ($spell eq 'drain' && $new_state->{mana} >= DRAIN_COST) {
+						$new_state->{mana} -= DRAIN_COST;
+						$new_state->{mana_used} += DRAIN_COST;
+						$new_state->{enemy_hp} -= DRAIN_DAMAGE;
+						$new_state->{hp} += 2;
+				}
+				elsif ($spell eq 'shield' && $new_state->{shield} == 0 && $new_state->{mana} >= SHIELD_COST) {
+						$new_state->{mana} -= SHIELD_COST;
+						$new_state->{mana_used} += SHIELD_COST;
+						$new_state->{shield} = SHIELD_TIME;
+				}
+				elsif ($spell eq 'poison' && $new_state->{poison} == 0 && $new_state->{mana} >= POISON_COST) {
+					$new_state->{mana} -= POISON_COST;
+					$new_state->{mana_used} += POISON_COST;
+					$new_state->{poison} = POISON_TIME;
+				}
+				elsif ($spell eq 'recharge' && $new_state->{recharge} == 0 && $new_state->{mana} >= RECHARGE_COST) {
+						$new_state->{mana} -= RECHARGE_COST;
+						$new_state->{mana_used} += RECHARGE_COST;
+						$new_state->{recharge} = RECHARGE_TIME;
+				}
+				else {
+					next;
+				}
+
+				if ($new_state->{enemy_hp} <= 0) {
+					if ($new_state->{mana_used} < $smallest_mana_used) {
+						$smallest_mana_used = $new_state->{mana_used};
+					}
+					next;
+				}
+
+				push @queue, $new_state;
+			}
+		}
+		elsif ($state->{player_turn} == 0) {
+			my $new_state = {
+				player_turn => 1,
+				hp => $state->{hp},
+				mana => $state->{mana},
+				enemy_hp => $state->{enemy_hp},
+				shield => $state->{shield},
+				poison => $state->{poison},
+				recharge => $state->{recharge},
+				mana_used => $state->{mana_used}
+			};
+
+			process_effects($new_state);
+
+			if ($new_state->{enemy_hp} <= 0) {
+				if ($new_state->{mana_used} < $smallest_mana_used) {
+					$smallest_mana_used = $new_state->{mana_used};
+				}
+				next;
+			}
+
+			my $damage = $enemy_damage;
+			if ($new_state->{shield} > 0) {
+				$damage -= SHIELD_ARMOUR;
+			}
+			$damage = 1 if $damage < 1;
+			$new_state->{hp} -= $damage;
+
+			if ($new_state->{hp} <= 0) {
+				next;
+			}
+
+			push @queue, $new_state;
+		}
 	}
 
-	foreach my $sp (@{&SPELLS}) {
-		if ($sp->{cost} > $pl->{mana}) {
-			# player can't afford spell
-			next;
-		}
-
-		my $player = dclone($pl);
-		my $enemy = dclone($en);
-
-		my $spell = dclone($sp); # need to avoid altering the source spell :(
-
-		# player turn
-		$player->{mana} -= $spell->{cost};
-		$player->{spent} += $spell->{cost};
-
-		if ($player->{spent} > $min_mana) {
-			next;
-		}
-
-		$player->{hit_points} += $spell->{heal};
-		my $damage = $spell->{damage};
-
-		# add effect if one was cast
-		if (exists($spell->{effect})) {
-			push @{$player->{effects}}, $spell->{effect};
-		}
-
-		# process effects
-		foreach my $effect (@{$player->{effects}}) {
-			my $action = $effect->{action};
-			if (exists($action->{damage})) {
-				$damage += $action->{damage};
-			}
-			elsif (exists($action->{mana})) {
-				$player->{mana} += $action->{mana};
-			}
-			elsif (exists($action->{armour})) {
-				$player->{armour} += $action->{armour};
-			}
-			$effect->{time}--;
-		}
-		$player->{effects} = [ grep { $_->{time} > 0 } @{$player->{effects}} ];
-
-		$enemy->{hit_points} -= $damage;
-		if ($enemy->{hit_points} <= 0) {
-			if ($player->{spent} < $min_mana) {
-				$min_mana = $player->{spent};
-			}
-			next;
-		}
-
-		# enemy turn
-		$damage = $enemy->{damage} - $player->{armour};
-		$damage = 1 if $damage < 1;
-
-		$player->{hit_points} -= $damage;
-		if ($player->{hit_points} <= 0) {
-			next;
-		}
-
-		$player->{armour} = 0;
-
-		fight($player, $enemy);
-	}
+	return $smallest_mana_used;
 }
