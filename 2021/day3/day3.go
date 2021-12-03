61fbd453e817516d51657bf2a163670f25ab3ae5
davidbell <david.bell@gmail.com>
2021-12-03 23:56:25 +0000

    moved some common code to a func

--- 2021/day3/day3.go
+++ 2021/day3/day3.go
@@ -15,18 +15,21 @@ func get_data() []string {
 	return lines
 }
 
+func count_at_pos(numbers []string, pos int) (int, int) {
+	ones := 0
+	for _, number := range numbers {
+		if number[pos] == '1' {
+			ones++
+		}
+	}
+	return len(numbers) - ones, ones
+}
+
 func find_number(numbers []string, most bool) string {
 	pos := 0
 	for len(numbers) > 1 {
-		ones := 0
-		zeroes := 0
-		for _, number := range numbers {
-			if number[pos] == '1' {
-				ones++
-			} else {
-				zeroes++
-			}
-		}
+		zeroes, ones := count_at_pos(numbers, pos)
+
 		var match byte
 		if most {
 			if ones >= zeroes {
@@ -57,31 +60,23 @@ func find_number(numbers []string, most bool) string {
 
 func main() {
 	numbers := get_data()
+
+	// part 1
 	width := len(numbers[0])
-	amount := len(numbers)
-
-	ones := make([]int, width)
-	for _, number := range numbers {
-		for i := 0; i < width; i++ {
-			if number[i] == '1' {
-				ones[i]++
-			}
-		}
-	}
-
 	gamma_str := ""
 	for i := 0; i < width; i++ {
-		if ones[i] > amount/2 {
+		zeroes, ones := count_at_pos(numbers, i)
+		if ones > zeroes {
 			gamma_str += "1"
 		} else {
 			gamma_str += "0"
 		}
 	}
-
 	gamma, _ := strconv.ParseInt(gamma_str, 2, 64)
 	epsilon := gamma ^ int64(math.Pow(2, float64(width))-1)
 	fmt.Println(gamma * epsilon)
 
+	// part 2
 	ogr_str := find_number(numbers, true)
 	ogr, _ := strconv.ParseInt(ogr_str, 2, 64)
 	csr_str := find_number(numbers, false)
