c4037131471ae35501eac1c8fc70d548857a79e8
David Bell <david.bell@gmail.com>
2021-12-03 18:16:04 +0000

    part2 done

--- 2021/day3/day3.go
+++ 2021/day3/day3.go
@@ -15,6 +15,46 @@ func get_data() []string {
 	return lines
 }
 
+func find_number(numbers []string, most bool) string {
+	pos := 0
+	for len(numbers) > 1 {
+		ones := 0
+		zeroes := 0
+		for _, number := range numbers {
+			if number[pos] == '1' {
+				ones++
+			} else {
+				zeroes++
+			}
+		}
+		var match byte
+		if most {
+			if ones >= zeroes {
+				match = '1'
+			} else {
+				match = '0'
+			}
+		} else {
+			if ones < zeroes {
+				match = '1'
+			} else {
+				match = '0'
+			}
+		}
+
+		new_numbers := []string{}
+		for _, number := range numbers {
+			if number[pos] == match {
+				new_numbers = append(new_numbers, number)
+			}
+		}
+
+		numbers = new_numbers
+		pos++
+	}
+	return numbers[0]
+}
+
 func main() {
 	numbers := get_data()
 	width := len(numbers[0])
@@ -41,4 +81,10 @@ func main() {
 	gamma, _ := strconv.ParseInt(gamma_str, 2, 64)
 	epsilon := gamma ^ int64(math.Pow(2, float64(width))-1)
 	fmt.Println(gamma * epsilon)
+
+	ogr_str := find_number(numbers, true)
+	ogr, _ := strconv.ParseInt(ogr_str, 2, 64)
+	csr_str := find_number(numbers, false)
+	csr, _ := strconv.ParseInt(csr_str, 2, 64)
+	fmt.Println(ogr * csr)
 }
