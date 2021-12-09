f2d28e9a7a0c9625cbae2afe1fa7b220770adb6f
davidbell <david.bell@gmail.com>
2021-12-09 23:15:58 +0000

    2021 day9 part2

--- 2021/day9/day9.go
+++ 2021/day9/day9.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
 )
 
@@ -36,24 +37,24 @@ func get_data() Grid {
 	return grid
 }
 
-func get_neighbours(grid Grid, pos Pos) []Pos {
-	neighbours := []Pos{}
+func get_neighbours(grid *Grid, pos *Pos) []*Pos {
+	neighbours := []*Pos{}
 	if pos.x > 0 {
-		neighbours = append(neighbours, Pos{x: pos.x - 1, y: pos.y, val: grid.squares[pos.y][pos.x-1]})
+		neighbours = append(neighbours, &Pos{x: pos.x - 1, y: pos.y, val: grid.squares[pos.y][pos.x-1]})
 	}
 	if pos.x < grid.width-1 {
-		neighbours = append(neighbours, Pos{x: pos.x + 1, y: pos.y, val: grid.squares[pos.y][pos.x+1]})
+		neighbours = append(neighbours, &Pos{x: pos.x + 1, y: pos.y, val: grid.squares[pos.y][pos.x+1]})
 	}
 	if pos.y > 0 {
-		neighbours = append(neighbours, Pos{x: pos.x, y: pos.y - 1, val: grid.squares[pos.y-1][pos.x]})
+		neighbours = append(neighbours, &Pos{x: pos.x, y: pos.y - 1, val: grid.squares[pos.y-1][pos.x]})
 	}
 	if pos.y < grid.height-1 {
-		neighbours = append(neighbours, Pos{x: pos.x, y: pos.y + 1, val: grid.squares[pos.y+1][pos.x]})
+		neighbours = append(neighbours, &Pos{x: pos.x, y: pos.y + 1, val: grid.squares[pos.y+1][pos.x]})
 	}
 	return neighbours
 }
 
-func lower_than_surrounding(grid Grid, pos Pos) bool {
+func lower_than_surrounding(grid *Grid, pos *Pos) bool {
 	neighbours := get_neighbours(grid, pos)
 	higher_neighbour := false
 	for _, neighbour := range neighbours {
@@ -66,17 +67,58 @@ func lower_than_surrounding(grid Grid, pos Pos) bool {
 	return higher_neighbour
 }
 
+func floodfill(grid *Grid, pos *Pos) int {
+	queue := []*Pos{pos}
+	visited := map[Pos]bool{}
+
+	size := 0
+
+	for len(queue) > 0 {
+		p := queue[0]
+		queue = queue[1:]
+
+		if p.val == 9 {
+			continue
+		}
+
+		if _, seen := visited[*p]; seen {
+			continue
+		}
+		visited[*p] = true
+
+		size++
+
+		neighbours := get_neighbours(grid, p)
+		queue = append(queue, neighbours...)
+	}
+
+	return size
+}
+
 func main() {
 	grid := get_data()
 
+	// part 1
+	low_points := []Pos{}
 	risk_sum := 0
 	for y := 0; y < grid.height; y++ {
 		for x := 0; x < grid.width; x++ {
 			pos := Pos{x: x, y: y, val: grid.squares[y][x]}
-			if lower_than_surrounding(grid, pos) {
+			if lower_than_surrounding(&grid, &pos) {
+				low_points = append(low_points, pos)
 				risk_sum += pos.val + 1
 			}
 		}
 	}
 	fmt.Println(risk_sum)
+
+	// part 2
+	sizes := []int{}
+	for _, point := range low_points {
+		size := floodfill(&grid, &point)
+		sizes = append(sizes, size)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(sizes)))
+	fmt.Println(sizes[0] * sizes[1] * sizes[2])
 }
