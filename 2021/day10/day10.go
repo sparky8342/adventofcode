9839dc0cc96ee461901ec47920427a533a7377dc
David Bell <david.bell@gmail.com>
2021-12-10 14:46:20 +0000

    tidy up

--- 2021/day10/day10.go
+++ 2021/day10/day10.go
@@ -15,7 +15,6 @@ func get_data() []string {
 }
 
 func main() {
-	left := map[string]bool{"(": true, "[": true, "{": true, "<": true}
 	right := map[string]string{")": "(", "]": "[", "}": "{", ">": "<"}
 	errors := map[string]int{")": 3, "]": 57, "}": 1197, ">": 25137}
 	complete := map[string]int{"(": 1, "[": 2, "{": 3, "<": 4}
@@ -25,33 +24,30 @@ func main() {
 	error_score := 0             // part 1
 	completion_scores := []int{} // part2
 
+line_loop:
 	for _, line := range lines {
 		stack := []string{}
 		for _, ru := range line {
 			char := string(ru)
-			if _, found := left[char]; found {
-				stack = append(stack, char)
-			} else {
-				matching := right[char]
+			if matching, found := right[char]; found {
 				if stack[len(stack)-1] != matching {
 					error_score += errors[char]
-					stack = []string{}
-					break
+					continue line_loop
 				} else {
 					stack = stack[:len(stack)-1]
 				}
+			} else {
+				stack = append(stack, char)
 			}
 		}
 
 		// part 2
-		if len(stack) > 0 {
-			complete_score := 0
-			for i := len(stack) - 1; i >= 0; i-- {
-				complete_score *= 5
-				complete_score += complete[stack[i]]
-			}
-			completion_scores = append(completion_scores, complete_score)
+		complete_score := 0
+		for i := len(stack) - 1; i >= 0; i-- {
+			complete_score *= 5
+			complete_score += complete[stack[i]]
 		}
+		completion_scores = append(completion_scores, complete_score)
 	}
 
 	fmt.Println(error_score)
