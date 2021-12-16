ca9850fd00a1205aa7d933109b2c34ef992eadaf
davidbell <david.bell@gmail.com>
2021-12-16 23:19:07 +0000

    2021 day16 part2

--- 2021/day16/day16.go
+++ 2021/day16/day16.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"strconv"
 	"strings"
 )
+
+var packet_version_total int64
+var bin_str string
 
 func get_data() string {
 	data, _ := ioutil.ReadFile("input.txt")
@@ -13,61 +17,115 @@ func get_data() string {
 	return line
 }
 
-func parse_bin(bin_str string, packet_version_total *int64) string {
-	// check for 0 padding at end
-	check, _ := strconv.ParseInt(bin_str, 2, 64)
-	if check == 0 {
-		return ""
-	}
-
+func parse_bin() *big.Int {
 	packet_version, _ := strconv.ParseInt(bin_str[0:3], 2, 64)
 	packet_type_id, _ := strconv.ParseInt(bin_str[3:6], 2, 64)
-	is_literal_value := packet_type_id == 4
 
-	*packet_version_total += packet_version
+	packet_version_total += packet_version
 
-	if is_literal_value {
+	var values []*big.Int
+	if packet_type_id != 4 {
+		length_type_id := bin_str[6]
+		if length_type_id == '0' {
+			length, _ := strconv.ParseInt(bin_str[7:22], 2, 64)
+			bin_str = bin_str[22:]
+			bin_length := len(bin_str)
+
+			for bin_length-len(bin_str) < int(length) {
+				// check for padding zeroes
+				check, _ := strconv.ParseInt(bin_str, 2, 64)
+				if check == 0 {
+					break
+				}
+				values = append(values, parse_bin())
+			}
+		} else {
+			sub_packets, _ := strconv.ParseInt(bin_str[7:18], 2, 64)
+			bin_str = bin_str[18:]
+			for len(values) < int(sub_packets) {
+				values = append(values, parse_bin())
+			}
+		}
+	}
+
+	switch packet_type_id {
+	case 0:
+		sum := big.NewInt(0)
+		for _, value := range values {
+			sum.Add(sum, value)
+		}
+		return sum
+	case 1:
+		product := big.NewInt(1)
+		for _, value := range values {
+			product.Mul(product, value)
+		}
+		return product
+	case 2:
+		min := big.NewInt(999999)
+		for _, value := range values {
+			if value.Cmp(min) == -1 {
+				min = value
+			}
+		}
+		return min
+	case 3:
+		max := big.NewInt(0)
+		for _, value := range values {
+			if value.Cmp(max) == 1 {
+				max = value
+			}
+		}
+		return max
+	case 4: // literal value
 		value_str := ""
 		i := 6
 		var start byte
 		start = '1'
 		for start == '1' {
 			start = bin_str[i]
-			value_str += bin_str[i+1:i+5]
+			value_str += bin_str[i+1 : i+5]
 			i += 5
 		}
 		value, _ := strconv.ParseInt(value_str, 2, 64)
-		_ = value
-		return bin_str[i:]
-	} else {
-		// is operator packet
-		length_type_id := bin_str[6]
-
-		if length_type_id == '0' {
-			return bin_str[22:]
-		} else if length_type_id == '1' {
-			return bin_str[18:]
+		bin_str = bin_str[i:]
+		return big.NewInt(value)
+	case 5:
+		if values[0].Cmp(values[1]) == 1 {
+			return big.NewInt(1)
+		} else {
+			return big.NewInt(0)
+		}
+	case 6:
+		if values[0].Cmp(values[1]) == -1 {
+			return big.NewInt(1)
+		} else {
+			return big.NewInt(0)
+		}
+	case 7:
+		if values[0].Cmp(values[1]) == 0 {
+			return big.NewInt(1)
+		} else {
+			return big.NewInt(0)
 		}
 	}
 
-	return ""
+	return big.NewInt(0)
 }
 
 func main() {
 	line := get_data()
 
-	bin_str := ""
+	bin_str = ""
 	for _, ru := range line {
 		num, _ := strconv.ParseUint(string(ru), 16, 32)
 		bin_str += fmt.Sprintf("%04b", num)
 	}
 
-	var packet_version_total int64
 	packet_version_total = 0
 
-	for len(bin_str) > 0 {
-		bin_str = parse_bin(bin_str, &packet_version_total)
-	}
+	val := parse_bin()
 
 	fmt.Println(packet_version_total)
+	fmt.Println(val)
 }
