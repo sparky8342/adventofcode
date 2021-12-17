f123f01b319ad28d6854b21b061ab3e48655bb25
davidbell <david.bell@gmail.com>
2021-12-17 01:06:21 +0000

    used struct instead of globals, some renaming and tidying up

--- 2021/day16/day16.go
+++ 2021/day16/day16.go
@@ -8,42 +8,38 @@ import (
 	"strings"
 )
 
-var packet_version_total int64
-var bin_str string
-
-func get_data() string {
-	data, _ := ioutil.ReadFile("input.txt")
-	line := strings.TrimSuffix(string(data), "\n")
-	return line
+type Transmission struct {
+	packet               string
+	packet_version_total int64
 }
 
-func parse_bin() *big.Int {
-	packet_version, _ := strconv.ParseInt(bin_str[0:3], 2, 64)
-	packet_type_id, _ := strconv.ParseInt(bin_str[3:6], 2, 64)
+func (tr *Transmission) parse() *big.Int {
+	packet_version, _ := strconv.ParseInt(tr.packet[0:3], 2, 64)
+	packet_type_id, _ := strconv.ParseInt(tr.packet[3:6], 2, 64)
 
-	packet_version_total += packet_version
+	tr.packet_version_total += packet_version
 
 	var values []*big.Int
 	if packet_type_id != 4 {
-		length_type_id := bin_str[6]
+		length_type_id := tr.packet[6]
 		if length_type_id == '0' {
-			length, _ := strconv.ParseInt(bin_str[7:22], 2, 64)
-			bin_str = bin_str[22:]
-			bin_length := len(bin_str)
+			length, _ := strconv.ParseInt(tr.packet[7:22], 2, 64)
+			tr.packet = tr.packet[22:]
+			bin_length := len(tr.packet)
 
-			for bin_length-len(bin_str) < int(length) {
+			for bin_length-len(tr.packet) < int(length) {
 				// check for padding zeroes
-				check, _ := strconv.ParseInt(bin_str, 2, 64)
+				check, _ := strconv.ParseInt(tr.packet, 2, 64)
 				if check == 0 {
 					break
 				}
-				values = append(values, parse_bin())
+				values = append(values, tr.parse())
 			}
 		} else {
-			sub_packets, _ := strconv.ParseInt(bin_str[7:18], 2, 64)
-			bin_str = bin_str[18:]
+			sub_packets, _ := strconv.ParseInt(tr.packet[7:18], 2, 64)
+			tr.packet = tr.packet[18:]
 			for len(values) < int(sub_packets) {
-				values = append(values, parse_bin())
+				values = append(values, tr.parse())
 			}
 		}
 	}
@@ -62,8 +58,14 @@ func parse_bin() *big.Int {
 		}
 		return product
 	case 2:
-		min := big.NewInt(999999)
+		min := big.NewInt(0)
+		first := true
 		for _, value := range values {
+			if first {
+				min = value
+				first = false
+				continue
+			}
 			if value.Cmp(min) == -1 {
 				min = value
 			}
@@ -83,12 +85,12 @@ func parse_bin() *big.Int {
 		var start byte
 		start = '1'
 		for start == '1' {
-			start = bin_str[i]
-			value_str += bin_str[i+1 : i+5]
+			start = tr.packet[i]
+			value_str += tr.packet[i+1 : i+5]
 			i += 5
 		}
 		value, _ := strconv.ParseInt(value_str, 2, 64)
-		bin_str = bin_str[i:]
+		tr.packet = tr.packet[i:]
 		return big.NewInt(value)
 	case 5:
 		if values[0].Cmp(values[1]) == 1 {
@@ -113,19 +115,23 @@ func parse_bin() *big.Int {
 	return big.NewInt(0)
 }
 
+func get_data() string {
+	data, _ := ioutil.ReadFile("input.txt")
+	line := strings.TrimSuffix(string(data), "\n")
+	return line
+}
+
 func main() {
 	line := get_data()
 
-	bin_str = ""
+	tr := Transmission{packet_version_total: 0}
 	for _, ru := range line {
 		num, _ := strconv.ParseUint(string(ru), 16, 32)
-		bin_str += fmt.Sprintf("%04b", num)
+		tr.packet += fmt.Sprintf("%04b", num)
 	}
 
-	packet_version_total = 0
+	val := tr.parse()
 
-	val := parse_bin()
-
-	fmt.Println(packet_version_total)
+	fmt.Println(tr.packet_version_total)
 	fmt.Println(val)
 }
