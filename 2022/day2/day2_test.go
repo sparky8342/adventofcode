7c0d9badbe0b102d4743f1f9f4031a06a1736993
davidbell <david.bell@gmail.com>
2022-12-03 01:19:57 +0000

    moved to 2 separate tests

--- 2022/day2/day2_test.go
+++ 2022/day2/day2_test.go
@@ -2,18 +2,20 @@ package main
 
 import "testing"
 
-func Test(t *testing.T) {
-	data := []string{"A Y", "B X", "C Z"}
+var data = []string{"A Y", "B X", "C Z"}
 
+func TestGetScore(t *testing.T) {
 	got_score := get_score(data)
 	want_score := 15
 
 	if got_score != want_score {
 		t.Errorf("got %d, wanted %d", got_score, want_score)
 	}
+}
 
-	got_score = get_score_with_strategy(data)
-	want_score = 12
+func TestGetScoreWithStrategy(t *testing.T) {
+	got_score := get_score_with_strategy(data)
+	want_score := 12
 
 	if got_score != want_score {
 		t.Errorf("got %d, wanted %d", got_score, want_score)
