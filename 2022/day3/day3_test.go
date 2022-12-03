615b0b158075805afdab01d8895b0c2cf5ad66d5
davidbell <david.bell@gmail.com>
2022-12-03 11:23:00 +0000

    2022 day3 part1

---
+++ 2022/day3/day3_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+var data = []string{
+	"vJrwpWtwJgWrhcsFMMfFFhFp",
+	"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+	"PmmdzqPrVvPwwTWBwg",
+	"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+	"ttgJtRGJQctTZtZT",
+	"CrZsJsPPZsGzwwsLwLmpwMDw",
+}
+
+func TestPrioritySum(t *testing.T) {
+	got_sum := priority_sum(data)
+	want_sum := 157
+
+	if got_sum != want_sum {
+		t.Errorf("got %d, wanted %d", got_sum, want_sum)
+	}
+}
