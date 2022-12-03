7b2334b6a9a8a5755a0aae2cf4f8c282ce7f3814
davidbell <david.bell@gmail.com>
2022-12-03 01:04:40 +0000

    moved test data inline

--- 2022/day1/day1_test.go
+++ 2022/day1/day1_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func Test(t *testing.T) {
-	data := load_data("input_test.txt")
+	data := "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n"
 
 	got_most, got_top_three := most_calories(data)
 	want_most, want_top_three := 24000, 45000
