9fe21d2267cbb789c6ab20847c630ede9d0ba226
davidbell <david.bell@gmail.com>
2022-12-04 12:58:32 +0000

    2022 day4 part2

--- 2022/day4/day4.go
+++ 2022/day4/day4.go
@@ -20,34 +20,64 @@ type Range struct {
 	end   int
 }
 
-func range_contains(data []string) int {
-	contains := 0
+type Pair struct {
+	range1 Range
+	range2 Range
+}
+
+func get_pairs(data []string) []Pair {
+	pairs := []Pair{}
 
 	for _, line := range data {
 		parts := strings.Split(line, ",")
-		range1 := strings.Split(parts[0], "-")
-		range2 := strings.Split(parts[1], "-")
+		range1_parts := strings.Split(parts[0], "-")
+		range2_parts := strings.Split(parts[1], "-")
 
-		range1_a, _ := strconv.Atoi(range1[0])
-		range1_b, _ := strconv.Atoi(range1[1])
-		range2_a, _ := strconv.Atoi(range2[0])
-		range2_b, _ := strconv.Atoi(range2[1])
+		range1_a, _ := strconv.Atoi(range1_parts[0])
+		range1_b, _ := strconv.Atoi(range1_parts[1])
+		range2_a, _ := strconv.Atoi(range2_parts[0])
+		range2_b, _ := strconv.Atoi(range2_parts[1])
 
-		ranges := []Range{
-			Range{start: range1_a, end: range1_b},
-			Range{start: range2_a, end: range2_b},
+		range1 := Range{start: range1_a, end: range1_b}
+		range2 := Range{start: range2_a, end: range2_b}
+
+		if range1.start > range2.start {
+			range1, range2 = range2, range1
 		}
 
-		if (ranges[0].start <= ranges[1].start && ranges[0].end >= ranges[1].end) || (ranges[1].start <= ranges[0].start && ranges[1].end >= ranges[0].end) {
+		pairs = append(pairs, Pair{range1: range1, range2: range2})
+	}
+
+	return pairs
+}
+
+func range_contains(pairs []Pair) int {
+	contains := 0
+	for _, pair := range pairs {
+		if (pair.range1.start <= pair.range2.start && pair.range1.end >= pair.range2.end) || (pair.range2.start <= pair.range1.start && pair.range2.end >= pair.range1.end) {
 			contains++
 		}
 	}
-
 	return contains
+}
+
+func range_overlaps(pairs []Pair) int {
+	overlaps := 0
+	for _, pair := range pairs {
+		if pair.range1.end >= pair.range2.start {
+			overlaps++
+		}
+	}
+	return overlaps
 }
 
 func main() {
 	data := load_data("input.txt")
-	contains := range_contains(data)
+	pairs := get_pairs(data)
+
+	contains := range_contains(pairs)
 	fmt.Println(contains)
+
+	overlaps := range_overlaps(pairs)
+	fmt.Println(overlaps)
 }
