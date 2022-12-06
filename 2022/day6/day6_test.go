c283bb60a2706570cc760520bdce8af48f15c36b
David Bell <david.bell@photobox.com>
2022-12-06 11:52:08 +0000

    2022 day 6 part 2

--- 2022/day6/day6_test.go
+++ 2022/day6/day6_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestExample1(t *testing.T) {
 	buffer := "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
-	got_marker := find_marker(buffer)
+	got_marker := find_marker(buffer, 4)
 	want_marker := 7
 
 	if got_marker != want_marker {
@@ -14,7 +14,7 @@ func TestExample1(t *testing.T) {
 
 func TestExample2(t *testing.T) {
 	buffer := "bvwbjplbgvbhsrlpgdmjqwftvncz"
-	got_marker := find_marker(buffer)
+	got_marker := find_marker(buffer, 4)
 	want_marker := 5
 
 	if got_marker != want_marker {
@@ -24,7 +24,7 @@ func TestExample2(t *testing.T) {
 
 func TestExample3(t *testing.T) {
 	buffer := "nppdvjthqldpwncqszvftbrmjlhg"
-	got_marker := find_marker(buffer)
+	got_marker := find_marker(buffer, 4)
 	want_marker := 6
 
 	if got_marker != want_marker {
@@ -34,7 +34,7 @@ func TestExample3(t *testing.T) {
 
 func TestExample4(t *testing.T) {
 	buffer := "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
-	got_marker := find_marker(buffer)
+	got_marker := find_marker(buffer, 4)
 	want_marker := 10
 
 	if got_marker != want_marker {
@@ -44,8 +44,58 @@ func TestExample4(t *testing.T) {
 
 func TestExample5(t *testing.T) {
 	buffer := "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
-	got_marker := find_marker(buffer)
+	got_marker := find_marker(buffer, 4)
 	want_marker := 11
+
+	if got_marker != want_marker {
+		t.Errorf("got %d, wanted %d", got_marker, want_marker)
+	}
+}
+
+func TestExample6(t *testing.T) {
+	buffer := "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+	got_marker := find_marker(buffer, 14)
+	want_marker := 19
+
+	if got_marker != want_marker {
+		t.Errorf("got %d, wanted %d", got_marker, want_marker)
+	}
+}
+
+func TestExample7(t *testing.T) {
+	buffer := "bvwbjplbgvbhsrlpgdmjqwftvncz"
+	got_marker := find_marker(buffer, 14)
+	want_marker := 23
+
+	if got_marker != want_marker {
+		t.Errorf("got %d, wanted %d", got_marker, want_marker)
+	}
+}
+
+func TestExample8(t *testing.T) {
+	buffer := "nppdvjthqldpwncqszvftbrmjlhg"
+	got_marker := find_marker(buffer, 14)
+	want_marker := 23
+
+	if got_marker != want_marker {
+		t.Errorf("got %d, wanted %d", got_marker, want_marker)
+	}
+}
+
+func TestExample9(t *testing.T) {
+	buffer := "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+	got_marker := find_marker(buffer, 14)
+	want_marker := 29
+
+	if got_marker != want_marker {
+		t.Errorf("got %d, wanted %d", got_marker, want_marker)
+	}
+}
+
+func TestExample10(t *testing.T) {
+	buffer := "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+	got_marker := find_marker(buffer, 14)
+	want_marker := 26
 
 	if got_marker != want_marker {
 		t.Errorf("got %d, wanted %d", got_marker, want_marker)
