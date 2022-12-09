ee414942d7e86e8a661039dc368cb87de1e4267a
David Bell <david.bell@photobox.com>
2022-12-09 12:26:18 +0000

    combined func for parts 1 and 2

--- 2022/day9/day9_test.go
+++ 2022/day9/day9_test.go
@@ -8,7 +8,7 @@ var data = []string{"R 4", "U 4", "L 3", "D 1", "R 4", "D 1", "L 5", "R 2"}
 
 func TestTailVisited(t *testing.T) {
 	commands := parse_data(data)
-	got_squares := run_commands_part1(commands)
+	got_squares := run_commands(commands, 1)
 	want_squares := 13
 
 	if got_squares != want_squares {
@@ -18,7 +18,7 @@ func TestTailVisited(t *testing.T) {
 
 func TestTailVisitedLongRope(t *testing.T) {
 	commands := parse_data(data)
-	got_squares := run_commands_part2(commands)
+	got_squares := run_commands(commands, 9)
 	want_squares := 1
 
 	if got_squares != want_squares {
