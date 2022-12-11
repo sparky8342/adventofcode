d5920975216d874d172a38684b0ac1a0e1a51179
davidbell <david.bell@gmail.com>
2022-12-11 12:57:16 +0000

    2022 day11 part1

---
+++ 2022/day11/day11.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Monkey struct {
+	id            int
+	items         []int
+	multiply_self bool
+	multiply      int
+	add           int
+	test          int
+	True          int
+	False         int
+	inspected     int
+}
+
+func load_data(filename string) string {
+	data, _ := ioutil.ReadFile(filename)
+	if data[len(data)-1] == '\n' {
+		data = data[:len(data)-1]
+	}
+	return string(data)
+}
+
+func parse_data(data string) []*Monkey {
+	monkeys := []*Monkey{}
+
+	sections := strings.Split(data, "\n\n")
+	for _, section := range sections {
+		lines := strings.Split(section, "\n")
+
+		monkey := Monkey{}
+
+		parts := strings.Split(lines[0], " ")
+		monkey.id = int(parts[1][0]) - '0'
+
+		parts = strings.Split(lines[1], " ")
+		for i := 4; i < len(parts); i++ {
+			str := parts[i]
+			if str[len(str)-1] == ',' {
+				str = str[:len(str)-1]
+			}
+			n, _ := strconv.Atoi(str)
+			monkey.items = append(monkey.items, n)
+		}
+
+		parts = strings.Split(lines[2], " ")
+		operand_str := parts[7]
+		if operand_str == "old" {
+			monkey.multiply_self = true
+		} else {
+			operand, _ := strconv.Atoi(parts[7])
+			if parts[6] == "*" {
+				monkey.multiply = operand
+				monkey.add = 0
+			} else if parts[6] == "+" {
+				monkey.multiply = 1
+				monkey.add = operand
+			}
+		}
+
+		parts = strings.Split(lines[3], " ")
+		monkey.test, _ = strconv.Atoi(parts[5])
+
+		parts = strings.Split(lines[4], " ")
+		monkey.True, _ = strconv.Atoi(parts[9])
+
+		parts = strings.Split(lines[5], " ")
+		monkey.False, _ = strconv.Atoi(parts[9])
+
+		monkeys = append(monkeys, &monkey)
+	}
+
+	return monkeys
+}
+
+func one_round(monkeys []*Monkey) {
+	for _, monkey := range monkeys {
+		for _, item := range monkey.items {
+			if monkey.multiply_self {
+				item = item * item
+			} else {
+				item = item*monkey.multiply + monkey.add
+			}
+			item /= 3
+
+			var dest_monkey *Monkey
+			if item%monkey.test == 0 {
+				dest_monkey = monkeys[monkey.True]
+			} else {
+				dest_monkey = monkeys[monkey.False]
+			}
+			dest_monkey.items = append(dest_monkey.items, item)
+
+			monkey.inspected++
+		}
+		monkey.items = []int{}
+	}
+}
+
+func main() {
+	data := load_data("input.txt")
+	monkeys := parse_data(data)
+
+	for i := 0; i < 20; i++ {
+		one_round(monkeys)
+	}
+
+	sort.Slice(monkeys, func(i, j int) bool {
+		return monkeys[i].inspected > monkeys[j].inspected
+	})
+
+	fmt.Println(monkeys[0].inspected * monkeys[1].inspected)
+}
