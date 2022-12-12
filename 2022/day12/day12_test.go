877e45e0f8d2c51b085d908c81dc042b119056a4
David Bell <david.bell@photobox.com>
2022-12-12 15:38:31 +0000

    2022 day 12 part 2

--- 2022/day12/day12_test.go
+++ 2022/day12/day12_test.go
@@ -15,3 +15,23 @@ func TestPath(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got_distance, want_distance)
 	}
 }
+
+func TestMinPath(t *testing.T) {
+	nodes, _, end := parse_data(data)
+
+	got_min_distance := 99999
+	for pos, node := range nodes {
+		if node.elevation == 'a' {
+			distance := bfs(nodes, pos, end)
+			if distance > 0 && distance < got_min_distance {
+				got_min_distance = distance
+			}
+		}
+	}
+
+	want_min_distance := 29
+
+	if got_min_distance != want_min_distance {
+		t.Errorf("got %d, wanted %d", got_min_distance, want_min_distance)
+	}
+}
