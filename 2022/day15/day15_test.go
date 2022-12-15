8db402766b552656b6f4da1b0cc89b76c549f96d
David Bell <david.bell@photobox.com>
2022-12-15 15:59:45 +0000

    2022 day 15 part 1

---
+++ 2022/day15/day15_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+var data = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3`
+
+const TEST_ROW = 10
+
+func TestSand(t *testing.T) {
+	area := parse_data(strings.Split(data, "\n"))
+
+	got_invalid := invalid_line(area, TEST_ROW)
+	want_invalid := 26
+
+	if got_invalid != want_invalid {
+		t.Errorf("got %d, wanted %d", got_invalid, want_invalid)
+	}
+}
