0010e90a92e81d1a8cea368507f6e30714831740
David Bell <david.bell@photobox.com>
2022-12-16 18:59:16 +0000

    2022 day 16 part 1

---
+++ 2022/day16/day16_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+var data = `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II`
+
+func TestPressure(t *testing.T) {
+	start, amount := parse_data(strings.Split(data, "\n"))
+
+	got_pressure := bfs(start, amount)
+	want_pressure := 1651
+
+	if got_pressure != want_pressure {
+		t.Errorf("got %d, wanted %d", got_pressure, want_pressure)
+	}
+}
