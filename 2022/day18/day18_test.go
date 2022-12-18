76a20972bd2e45342e6786fca992c292f7571e8a
davidbell <david.bell@gmail.com>
2022-12-18 13:51:44 +0000

    2022 day 18 part 2

--- 2022/day18/day18_test.go
+++ 2022/day18/day18_test.go
@@ -20,11 +20,23 @@ const data = `2,2,2
 2,3,5`
 
 func TestSides(t *testing.T) {
-	cubes := parse_data(strings.Split(data, "\n"))
-	got_sides := count_sides(cubes)
+	droplet := parse_data(strings.Split(data, "\n"))
+	got_sides := count_sides(droplet)
 	want_sides := 64
 
 	if got_sides != want_sides {
 		t.Errorf("got %d, wanted %d", got_sides, want_sides)
+	}
+}
+
+func TestArea(t *testing.T) {
+	droplet := parse_data(strings.Split(data, "\n"))
+	sides := count_sides(droplet)
+	inside_sides := count_holes(droplet)
+	got_area := sides - inside_sides
+	want_area := 58
+
+	if got_area != want_area {
+		t.Errorf("got %d, wanted %d", got_area, want_area)
 	}
 }
