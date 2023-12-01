4139225db43d85fb64078a865510eb940b1f23dc
David Bell <david.bell@photobox.com>
2023-12-01 11:37:54 +0000

    2023 day 1 part 2

--- 2023/day1/day1_test.go
+++ 2023/day1/day1_test.go
@@ -16,4 +16,21 @@ func Test(t *testing.T) {
 	if got_calibration != want_calibration {
 		t.Errorf("got %d, wanted %d", got_calibration, want_calibration)
 	}
+
+	data = []string{
+		"two1nine",
+		"eightwothree",
+		"abcone2threexyz",
+		"xtwone3four",
+		"4nineeightseven2",
+		"zoneight234",
+		"7pqrstsixteen",
+	}
+
+	got_calibration2 := calibration2(data)
+	want_calibration2 := 281
+
+	if got_calibration2 != want_calibration2 {
+		t.Errorf("got %d, wanted %d", got_calibration2, want_calibration2)
+	}
 }
