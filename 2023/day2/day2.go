95d9cb93dda91f4fc659138f57e8e74ccbca5c07
David Bell <david.bell@gmail.com>
2023-12-02 11:55:22 +0000

    parse data separately

--- 2023/day2/day2.go
+++ 2023/day2/day2.go
@@ -7,6 +7,24 @@ import (
 	"strings"
 )
 
+var max_allowed Set
+
+type Set struct {
+	cube map[string]int
+}
+
+type Game struct {
+	id      int
+	sets    []Set
+	max_set Set
+	valid   bool
+	power   int
+}
+
+func init() {
+	max_allowed = Set{cube: map[string]int{"red": 12, "green": 13, "blue": 14}}
+}
+
 func load_data(filename string) []string {
 	data, _ := ioutil.ReadFile(filename)
 	if data[len(data)-1] == '\n' {
@@ -15,55 +33,57 @@ func load_data(filename string) []string {
 	return strings.Split(string(data), "\n")
 }
 
+func parse_line(line string) Game {
+	game := Game{
+		valid:   true,
+		power:   1,
+		max_set: Set{cube: map[string]int{"red": 0, "green": 0, "blue": 0}},
+	}
+
+	parts := strings.Split(line, ": ")
+	prefix := strings.Split(parts[0], " ")
+	id, _ := strconv.Atoi(prefix[1])
+	game.id = id
+
+	sets_str := strings.Split(parts[1], "; ")
+
+	for _, set_str := range sets_str {
+		set := Set{cube: map[string]int{}}
+		cube_strs := strings.Split(set_str, ", ")
+
+		for _, cube_str := range cube_strs {
+			cube_str_parts := strings.Split(cube_str, " ")
+			amount_str, colour := cube_str_parts[0], cube_str_parts[1]
+			amount, _ := strconv.Atoi(amount_str)
+			set.cube[colour] = amount
+
+			if amount > game.max_set.cube[colour] {
+				game.max_set.cube[colour] = amount
+			}
+			if amount > max_allowed.cube[colour] {
+				game.valid = false
+			}
+		}
+		game.sets = append(game.sets, set)
+	}
+
+	for _, amount := range game.max_set.cube {
+		game.power *= amount
+	}
+
+	return game
+}
+
 func possible(data []string) (int, int) {
 	id_total := 0
 	power_total := 0
 
-	max := map[string]int{
-		"red":   12,
-		"green": 13,
-		"blue":  14,
-	}
-
 	for _, line := range data {
-		parts := strings.Split(line, ": ")
-		prefix := strings.Split(parts[0], " ")
-		id, _ := strconv.Atoi(prefix[1])
-
-		sets := strings.Split(parts[1], "; ")
-
-		valid := true
-		max_found := map[string]int{"red": 0, "green": 0, "blue": 0}
-
-		for _, set := range sets {
-			cube_strs := strings.Split(set, ", ")
-			cubes := map[string]int{}
-			for _, cube_str := range cube_strs {
-				cube_str_parts := strings.Split(cube_str, " ")
-				n, _ := strconv.Atoi(cube_str_parts[0])
-				cubes[cube_str_parts[1]] = n
-			}
-
-			for key, value := range cubes {
-				if value > max[key] {
-					valid = false
-				}
-				if value > max_found[key] {
-					max_found[key] = value
-				}
-			}
+		game := parse_line(line)
+		if game.valid {
+			id_total += game.id
 		}
-
-		if valid {
-			id_total += id
-		}
-
-		power := 1
-		for _, value := range max_found {
-			power *= value
-		}
-		power_total += power
-
+		power_total += game.power
 	}
 
 	return id_total, power_total
