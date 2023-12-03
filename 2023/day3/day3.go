f87f03e9238dc0c83c346b1cb8e6e5660ac1d048
David Bell <david.bell@gmail.com>
2023-12-03 12:58:42 +0000

    2023 day 3 part 2

--- 2023/day3/day3.go
+++ 2023/day3/day3.go
@@ -6,6 +6,15 @@ import (
 	"strings"
 )
 
+type Pos struct {
+	x, y int
+}
+
+type Num struct {
+	value    int
+	star_pos Pos
+}
+
 func load_data(filename string) []string {
 	data, _ := ioutil.ReadFile(filename)
 	if data[len(data)-1] == '\n' {
@@ -18,7 +27,7 @@ func is_digit(b byte) bool {
 	return b >= '0' && b <= '9'
 }
 
-func adjacent_to_symbol(data []string, x int, x2 int, y int) bool {
+func adjacent_to_symbol(data []string, x int, x2 int, y int) (bool, int, int) {
 	height := len(data)
 	width := len(data[0])
 
@@ -27,24 +36,30 @@ func adjacent_to_symbol(data []string, x int, x2 int, y int) bool {
 			continue
 		}
 		if y > 0 && data[y-1][dx] != '.' {
-			return true
+			return true, dx, y - 1
 		}
 		if y < height-1 && data[y+1][dx] != '.' {
-			return true
+			return true, dx, y + 1
 		}
 	}
-	if (x > 0 && data[y][x-1] != '.') || (x2 < width-1 && data[y][x2+1] != '.') {
-		return true
+	if x > 0 && data[y][x-1] != '.' {
+		return true, x - 1, y
 	}
 
-	return false
+	if x2 < width-1 && data[y][x2+1] != '.' {
+		return true, x2 + 1, y
+	}
+
+	return false, -1, -1
 }
 
-func part_sum(data []string) int {
+func sum_and_ratio(data []string) (int, int) {
 	height := len(data)
 	width := len(data[0])
 
 	sum := 0
+
+	nums := []Num{}
 
 	for y := 0; y < height; y++ {
 		x := 0
@@ -63,23 +78,43 @@ func part_sum(data []string) int {
 				continue
 			}
 
-			if adjacent_to_symbol(data, x, x2, y) {
+			is_adj, adj_x, adj_y := adjacent_to_symbol(data, x, x2, y)
+			if is_adj {
 				num := int(data[y][x] - '0')
 				for i := x + 1; i <= x2; i++ {
 					num = num*10 + int(data[y][i]-'0')
 				}
 				sum += num
+
+				if data[adj_y][adj_x] == '*' {
+					nums = append(nums, Num{
+						value:    num,
+						star_pos: Pos{x: adj_x, y: adj_y},
+					})
+				}
 			}
 
 			x = x2 + 1
 		}
 	}
 
-	return sum
+	ratio := 0
+
+	seen := map[Pos]int{}
+	for _, n := range nums {
+		if val, ok := seen[n.star_pos]; ok {
+			ratio += val * n.value
+		} else {
+			seen[n.star_pos] = n.value
+		}
+	}
+
+	return sum, ratio
 }
 
 func main() {
 	data := load_data("input.txt")
-	fmt.Println(part_sum(data))
-
+	sum, ratio := sum_and_ratio(data)
+	fmt.Println(sum)
+	fmt.Println(ratio)
 }
