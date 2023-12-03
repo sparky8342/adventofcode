99c5bc334830f1f6aa8f8b5d01a1a3f23e2aaba7
David Bell <david.bell@gmail.com>
2023-12-03 12:58:42 +0000

    2023 day 3 part 2

--- 2023/day3/day3_test.go
+++ 2023/day3/day3_test.go
@@ -16,10 +16,15 @@ func Test(t *testing.T) {
 		".664.598..",
 	}
 
-	got_sum := part_sum(data)
+	got_sum, got_ratio := sum_and_ratio(data)
 	want_sum := 4361
+	want_ratio := 467835
 
 	if got_sum != want_sum {
 		t.Errorf("got %d, wanted %d", got_sum, want_sum)
+	}
+
+	if got_ratio != want_ratio {
+		t.Errorf("got %d, wanted %d", got_ratio, want_ratio)
 	}
 }
