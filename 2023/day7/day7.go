f392c23ecb52b24ef86a2df9eec5e9daacd756c2
David Bell <david.bell@photobox.com>
2023-12-07 13:02:32 +0000

    2023 day 7 part 2

--- 2023/day7/day7.go
+++ 2023/day7/day7.go
@@ -21,6 +21,7 @@ const (
 type Hand struct {
 	cards string
 	typ   int
+	typ2  int
 	bid   int
 }
 
@@ -32,9 +33,9 @@ func load_data(filename string) []string {
 	return strings.Split(string(data), "\n")
 }
 
-func get_type(hand string) int {
+func get_type(cards string) int {
 	distinct_cards := map[rune]int{}
-	for _, ru := range hand {
+	for _, ru := range cards {
 		distinct_cards[ru]++
 	}
 
@@ -76,7 +77,7 @@ func get_type(hand string) int {
 	return -1
 }
 
-func card_to_num(card byte) int {
+func card_to_num(card byte, wild bool) int {
 	switch card {
 	case 'A':
 		{
@@ -92,7 +93,11 @@ func card_to_num(card byte) int {
 		}
 	case 'J':
 		{
-			return 11
+			if wild {
+				return 1
+			} else {
+				return 11
+			}
 		}
 	case 'T':
 		{
@@ -105,43 +110,86 @@ func card_to_num(card byte) int {
 	}
 }
 
+func convert_wildcards(cards string) string {
+	card_counts := map[rune]int{}
+	var highest_card rune
+	highest_amount := 0
+
+	for _, ru := range cards {
+		card_counts[ru]++
+		if ru != 'J' && card_counts[ru] > highest_amount {
+			highest_amount = card_counts[ru]
+			highest_card = ru
+		}
+	}
+
+	if _, exists := card_counts['J']; !exists {
+		return cards
+	}
+
+	bytes := []byte(cards)
+	b := byte(highest_card)
+	for i := 0; i < 5; i++ {
+		if bytes[i] == 'J' {
+			bytes[i] = b
+		}
+	}
+
+	return string(bytes)
+}
+
 func parse_data(data []string) []Hand {
 	hands := []Hand{}
 	for _, line := range data {
 		parts := strings.Fields(line)
 		bid, _ := strconv.Atoi(parts[1])
-		hands = append(hands, Hand{cards: parts[0], typ: get_type(parts[0]), bid: bid})
+		hands = append(hands, Hand{
+			cards: parts[0],
+			typ:   get_type(parts[0]),
+			typ2:  get_type(convert_wildcards(parts[0])),
+			bid:   bid,
+		})
 	}
 	return hands
 }
 
-func winnings(hands []Hand) int {
-	sort.Slice(hands, func(i, j int) bool {
-		if hands[i].typ == hands[j].typ {
-			for k := 0; k < 5; k++ {
-				a := card_to_num(hands[i].cards[k])
-				b := card_to_num(hands[j].cards[k])
-				if a != b {
-					return a > b
-				}
-			}
-			return true
-		} else {
-			return hands[i].typ > hands[j].typ
-		}
-	})
+func winnings(hands []Hand) (int, int) {
+	scores := make([]int, 2)
 
-	score := 0
-	for i := 0; i < len(hands); i++ {
-		score += (len(hands) - i) * hands[i].bid
+	for typ := 0; typ <= 1; typ++ {
+		wild := false
+		if typ == 1 {
+			wild = true
+		}
+		sort.Slice(hands, func(i, j int) bool {
+			if (typ == 0 && hands[i].typ == hands[j].typ) || (typ == 1 && hands[i].typ2 == hands[j].typ2) {
+				for k := 0; k < 5; k++ {
+					a := card_to_num(hands[i].cards[k], wild)
+					b := card_to_num(hands[j].cards[k], wild)
+					if a != b {
+						return a > b
+					}
+				}
+				return true
+			} else {
+				return (typ == 0 && hands[i].typ > hands[j].typ) || (typ == 1 && hands[i].typ2 > hands[j].typ2)
+			}
+		})
+
+		score := 0
+		for i := 0; i < len(hands); i++ {
+			score += (len(hands) - i) * hands[i].bid
+		}
+		scores[typ] = score
 	}
 
-	return score
+	return scores[0], scores[1]
 }
 
 func main() {
 	data := load_data("input.txt")
 	hands := parse_data(data)
-	score := winnings(hands)
+	score, wildcard_score := winnings(hands)
 	fmt.Println(score)
+	fmt.Println(wildcard_score)
 }
