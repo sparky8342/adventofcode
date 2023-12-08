5b9d4ca84180f2d45714941ff3e82c36ac988149
David Bell <david.bell@photobox.com>
2023-12-08 13:38:11 +0000

    2023 day 8 part 2

--- 2023/day8/day8.go
+++ 2023/day8/day8.go
@@ -36,7 +36,6 @@ func parse_data(data []string) (string, map[string]Node) {
 
 func follow_instructions(instructions string, nodes map[string]Node) int {
 	pos := "AAA"
-
 	steps := 0
 
 	for {
@@ -54,9 +53,72 @@ func follow_instructions(instructions string, nodes map[string]Node) int {
 	}
 }
 
+func find_cycle(position string, instructions string, nodes map[string]Node) (int, int) {
+	seen := map[string]int{}
+	step_count := 0
+
+	for {
+		for _, ins := range instructions {
+			if ins == 'L' {
+				position = nodes[position].left
+			} else if ins == 'R' {
+				position = nodes[position].right
+			}
+			step_count++
+			if position[2] == 'Z' {
+				if val, exists := seen[position]; exists {
+					return step_count, step_count - val
+				} else {
+					seen[position] = step_count
+				}
+			}
+		}
+	}
+}
+
+func all_same(nums []int) bool {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[0] {
+			return false
+		}
+	}
+	return true
+}
+
+func ghost_steps(instructions string, nodes map[string]Node) int {
+	positions := []string{}
+	for name, _ := range nodes {
+		if name[2] == 'A' {
+			positions = append(positions, name)
+		}
+	}
+
+	steps := make([]int, len(positions))
+	cycles := make([]int, len(positions))
+
+	for i, position := range positions {
+		steps[i], cycles[i] = find_cycle(position, instructions, nodes)
+	}
+
+	for {
+		smallest := 0
+		for i := 1; i < len(steps); i++ {
+			if steps[i] < steps[smallest] {
+				smallest = i
+			}
+		}
+		steps[smallest] += cycles[smallest]
+		if all_same(steps) {
+			return steps[0]
+		}
+	}
+}
+
 func main() {
 	data := load_data("input.txt")
 	instructions, nodes := parse_data(data)
 	steps := follow_instructions(instructions, nodes)
+	fmt.Println(steps)
+	steps = ghost_steps(instructions, nodes)
 	fmt.Println(steps)
 }
--- 2023/day8/day8_test.go
+++ 2023/day8/day8_test.go
@@ -40,4 +40,26 @@ func Test(t *testing.T) {
 	if got_steps != want_steps {
 		t.Errorf("got %d, wanted %d", got_steps, want_steps)
 	}
+
+	data = []string{
+		"LR",
+		"",
+		"11A = (11B, XXX)",
+		"11B = (XXX, 11Z)",
+		"11Z = (11B, XXX)",
+		"22A = (22B, XXX)",
+		"22B = (22C, 22C)",
+		"22C = (22Z, 22Z)",
+		"22Z = (22B, 22B)",
+		"XXX = (XXX, XXX)",
+	}
+
+	instructions, nodes = parse_data(data)
+
+	got_steps = ghost_steps(instructions, nodes)
+	want_steps = 6
+
+	if got_steps != want_steps {
+		t.Errorf("got %d, wanted %d", got_steps, want_steps)
+	}
 }
