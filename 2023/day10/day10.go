095dc1fde61167a3fcda6697814b80138b3d5308
David Bell <david.bell@gmail.com>
2023-12-10 21:28:19 +0000

    2023 day 10 part 2 (pretty messy)

--- 2023/day10/day10.go
+++ 2023/day10/day10.go
@@ -13,28 +13,33 @@ type Pos struct {
 	dy int
 }
 
+type Square struct {
+	x int
+	y int
+}
+
+type Empty struct {
+}
+
 var height, width int
 
-func load_data(filename string) []string {
+func get_data(filename string) [][]byte {
 	data, _ := ioutil.ReadFile(filename)
 	if data[len(data)-1] == '\n' {
 		data = data[:len(data)-1]
 	}
-	return strings.Split(string(data), "\n")
-}
-
-func get_at(data []string, pos Pos) byte {
-	if pos.x < 0 || pos.x == width || pos.y < 0 || pos.y > height {
-		return '.'
-	} else {
-		return data[pos.y][pos.x]
+	lines := strings.Split(string(data), "\n")
+	grid := [][]byte{}
+	for _, line := range lines {
+		grid = append(grid, []byte(line))
 	}
+	return grid
 }
 
-func find_start(data []string) Pos {
+func find_start(grid [][]byte) Pos {
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			if data[y][x] == 'S' {
+			if grid[y][x] == 'S' {
 				return Pos{x: x, y: y}
 			}
 		}
@@ -42,9 +47,9 @@ func find_start(data []string) Pos {
 	return Pos{x: -1, y: -1}
 }
 
-func connects(data []string, pos Pos) Pos {
+func connects(grid [][]byte, pos Pos) Pos {
 	north_pos := Pos{x: pos.x, y: pos.y - 1, dx: 0, dy: -1}
-	north := get_at(data, north_pos)
+	north := grid[pos.y-1][pos.x]
 	if north == '|' || north == '7' || north == 'F' {
 		if north == '7' {
 			north_pos.dx = -1
@@ -57,7 +62,7 @@ func connects(data []string, pos Pos) Pos {
 	}
 
 	east_pos := Pos{x: pos.x + 1, y: pos.y, dx: 1, dy: 0}
-	east := get_at(data, east_pos)
+	east := grid[pos.y][pos.x+1]
 	if east == '-' || east == 'J' || east == '7' {
 		if east == 'J' {
 			east_pos.dx = 0
@@ -70,7 +75,7 @@ func connects(data []string, pos Pos) Pos {
 	}
 
 	south_pos := Pos{x: pos.x, y: pos.y + 1, dx: 0, dy: 1}
-	south := get_at(data, south_pos)
+	south := grid[pos.y+1][pos.x]
 	if south == '|' || south == 'L' || south == 'J' {
 		if south == 'L' {
 			south_pos.dx = 1
@@ -83,7 +88,7 @@ func connects(data []string, pos Pos) Pos {
 	}
 
 	west_pos := Pos{x: pos.x - 1, y: pos.y, dx: -1, dy: 0}
-	west := get_at(data, west_pos)
+	west := grid[pos.y][pos.x-1]
 	if west == '-' || west == 'L' || west == 'F' {
 		if west == 'L' {
 			west_pos.dx = 0
@@ -98,11 +103,11 @@ func connects(data []string, pos Pos) Pos {
 	return Pos{}
 }
 
-func move(data []string, pos Pos) Pos {
+func move(grid [][]byte, pos Pos) Pos {
 	pos.x += pos.dx
 	pos.y += pos.dy
 
-	b := data[pos.y][pos.x]
+	b := grid[pos.y][pos.x]
 	if pos.dx == 1 {
 		// going east
 		if b == 'J' {
@@ -143,24 +148,78 @@ func move(data []string, pos Pos) Pos {
 	return pos
 }
 
-func distance(data []string) int {
-	height = len(data)
-	width = len(data[0])
+func is_inside(grid [][]byte, path map[Square]Empty, x int, y int) bool {
+	if _, exists := path[Square{x: x, y: y}]; exists {
+		return false
+	}
 
-	start := find_start(data)
+	lx := x - 1
+	crosses := 0
+	for lx >= 0 {
+		if _, exists := path[Square{x: lx, y: y}]; exists {
+			if grid[y][lx] == '|' || grid[y][lx] == 'J' || grid[y][lx] == 'L' {
+				crosses++
+			}
+		}
+		lx--
+	}
+	return crosses%2 == 1
+}
 
-	pos := connects(data, start)
+func count_inside(grid [][]byte, path map[Square]Empty) int {
+	inside := 0
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if is_inside(grid, path, x, y) {
+				inside++
+			}
+		}
+	}
+	return inside
+}
 
+func path_info(grid [][]byte) (int, int) {
+	height = len(grid)
+	width = len(grid[0])
+
+	path := map[Square]Empty{}
+
+	start := find_start(grid)
+	corners := []Pos{}
+
+	path[Square{x: start.x, y: start.y}] = Empty{}
+
+	first_pos := connects(grid, start)
+	if grid[first_pos.y][first_pos.x] != '|' && grid[first_pos.y][first_pos.x] != '-' {
+		corners = append(corners, first_pos)
+	}
+	path[Square{x: first_pos.x, y: first_pos.y}] = Empty{}
+
+	pos := first_pos
 	steps := 1
 	for !(pos.x == start.x && pos.y == start.y) {
-		pos = move(data, pos)
+		pos = move(grid, pos)
+		if grid[pos.y][pos.x] != '|' && grid[pos.y][pos.x] != '-' {
+			corners = append(corners, pos)
+		}
+
+		path[Square{x: pos.x, y: pos.y}] = Empty{}
 		steps++
 	}
 
-	return steps / 2
+	inside := count_inside(grid, path)
+	return steps / 2, inside
+}
+
+func pp(grid [][]byte) {
+	for _, row := range grid {
+		fmt.Println(string(row))
+	}
 }
 
 func main() {
-	data := load_data("input.txt")
-	fmt.Println(distance(data))
+	grid := get_data("input.txt")
+	distance, inside := path_info(grid)
+	fmt.Println(distance)
+	fmt.Println(inside)
 }
--- 2023/day10/day10_test.go
+++ 2023/day10/day10_test.go
@@ -3,18 +3,76 @@ package main
 import "testing"
 
 func Test(t *testing.T) {
-	data := []string{
-		"-L|F7",
-		"7S-7|",
-		"L|7||",
-		"-L-J|",
-		"L|-JF",
+	data := [][]byte{
+		[]byte("-L|F7"),
+		[]byte("7S-7|"),
+		[]byte("L|7||"),
+		[]byte("-L-J|"),
+		[]byte("L|-JF"),
 	}
 
-	got_distance := distance(data)
+	got_distance, _ := path_info(data)
 	want_distance := 4
 
 	if got_distance != want_distance {
 		t.Errorf("got %d, wanted %d", got_distance, want_distance)
+	}
+
+	data = [][]byte{
+		[]byte("..........."),
+		[]byte(".S-------7."),
+		[]byte(".|F-----7|."),
+		[]byte(".||.....||."),
+		[]byte(".||.....||."),
+		[]byte(".|L-7.F-J|."),
+		[]byte(".|..|.|..|."),
+		[]byte(".L--J.L--J."),
+		[]byte("..........."),
+	}
+
+	_, got_inside := path_info(data)
+	want_inside := 4
+
+	if got_inside != want_inside {
+		t.Errorf("got %d, wanted %d", got_inside, want_inside)
+	}
+
+	data = [][]byte{
+		[]byte(".........."),
+		[]byte(".S------7."),
+		[]byte(".|F----7|."),
+		[]byte(".||....||."),
+		[]byte(".||....||."),
+		[]byte(".|L-7F-J|."),
+		[]byte(".|..||..|."),
+		[]byte(".L--JL--J."),
+		[]byte(".........."),
+	}
+
+	_, got_inside = path_info(data)
+	want_inside = 4
+
+	if got_inside != want_inside {
+		t.Errorf("got %d, wanted %d", got_inside, want_inside)
+	}
+
+	data = [][]byte{
+		[]byte(".F----7F7F7F7F-7...."),
+		[]byte(".|F--7||||||||FJ...."),
+		[]byte(".||.FJ||||||||L7...."),
+		[]byte("FJL7L7LJLJ||LJ.L-7.."),
+		[]byte("L--J.L7...LJS7F-7L7."),
+		[]byte("....F-J..F7FJ|L7L7L7"),
+		[]byte("....L7.F7||L7|.L7L7|"),
+		[]byte(".....|FJLJ|FJ|F7|.LJ"),
+		[]byte("....FJL-7.||.||||..."),
+		[]byte("....L---J.LJ.LJLJ..."),
+	}
+
+	_, got_inside = path_info(data)
+	want_inside = 8
+
+	if got_inside != want_inside {
+		t.Errorf("got %d, wanted %d", got_inside, want_inside)
 	}
 }
