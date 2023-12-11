344e63f1b6696ffae93d8809cd318e983bc841c6
David Bell <david.bell@photobox.com>
2023-12-11 13:24:57 +0000

    2023 day 11 part 2 - don't expand the universe, just calculate the expansion

--- 2023/day11/day11_test.go
+++ 2023/day11/day11_test.go
@@ -17,10 +17,23 @@ func Test(t *testing.T) {
 	}
 
 	universe := parse_data(data)
-	universe.expand()
 
-	got_sum := universe.path_sum()
+	got_sum := universe.path_sum(2)
 	want_sum := 374
+
+	if got_sum != want_sum {
+		t.Errorf("got %d, wanted %d", got_sum, want_sum)
+	}
+
+	got_sum = universe.path_sum(10)
+	want_sum = 1030
+
+	if got_sum != want_sum {
+		t.Errorf("got %d, wanted %d", got_sum, want_sum)
+	}
+
+	got_sum = universe.path_sum(100)
+	want_sum = 8410
 
 	if got_sum != want_sum {
 		t.Errorf("got %d, wanted %d", got_sum, want_sum)
