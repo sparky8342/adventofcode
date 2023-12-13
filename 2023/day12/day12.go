1ae6aff2eb9c571ff8bfc354e8574897129b8213
David Bell <david.bell@photobox.com>
2023-12-13 22:45:22 +0000

    back to backtracking version

--- 2023/day12/day12.go
+++ 2023/day12/day12.go
@@ -3,21 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
+	"sync"
 )
-
-type CacheKey struct {
-	seq_i   int
-	size_i  int
-	current int
-}
-
-var cache map[CacheKey]int
-
-func init() {
-	cache = map[CacheKey]int{}
-}
 
 func load_data(filename string) []string {
 	data, _ := ioutil.ReadFile(filename)
@@ -27,61 +17,120 @@ func load_data(filename string) []string {
 	return strings.Split(string(data), "\n")
 }
 
-// thanks to jonathan paulson for this algorithm
-func combinations(sequence []byte, sizes []int, seq_i int, size_i int, current int) int {
-	key := CacheKey{seq_i: seq_i, size_i: size_i, current: current}
-	if value, exists := cache[key]; exists {
-		return value
-	}
+func is_valid(sequence []byte, sizes []int) bool {
+	found := []int{}
 
-	if seq_i == len(sequence) {
-		if size_i == len(sizes) && current == 0 {
-			return 1
-		} else if size_i == len(sizes)-1 && sizes[size_i] == current {
-			return 1
-		} else {
-			return 0
+	current := 0
+	all_scanned := true
+	for _, b := range sequence {
+		if b == '?' {
+			all_scanned = false
+			break
+		} else if b == '#' {
+			current++
+		} else if b == '.' && current > 0 {
+			found = append(found, current)
+			current = 0
 		}
 	}
+	if current > 0 {
+		found = append(found, current)
+	}
 
-	ans := 0
-	for _, c := range []byte{'.', '#'} {
-		if sequence[seq_i] == c || sequence[seq_i] == '?' {
-			if c == '.' && current == 0 {
-				ans += combinations(sequence, sizes, seq_i+1, size_i, 0)
-			} else if c == '.' && current > 0 && size_i < len(sizes) && sizes[size_i] == current {
-				ans += combinations(sequence, sizes, seq_i+1, size_i+1, 0)
-			} else if c == '#' {
-				ans += combinations(sequence, sizes, seq_i+1, size_i, current+1)
+	if len(found) > len(sizes) {
+		return false
+	}
+
+	if all_scanned {
+		if len(found) != len(sizes) {
+			return false
+		}
+		for i := 0; i < len(found); i++ {
+			if found[i] != sizes[i] {
+				return false
+			}
+		}
+		return true
+
+	} else if len(found) > 0 {
+		last_found := len(found) - 1
+		if found[last_found] > sizes[last_found] {
+			return false
+		}
+
+		for i := 0; i < last_found-1; i++ {
+			if found[i] != sizes[i] {
+				return false
 			}
 		}
 	}
 
-	cache[key] = ans
-	return ans
+	return true
 }
 
-func unfold(sequence []byte, sizes []int) ([]byte, []int) {
-	unfolded_sequence := sequence
-	for i := 0; i < 4; i++ {
-		unfolded_sequence = append(unfolded_sequence, '?')
-		unfolded_sequence = append(unfolded_sequence, sequence...)
+func backtrack(sequence []byte, sizes []int) int {
+	valid := 0
+
+	wildcards := []int{}
+	for i, b := range sequence {
+		if b == '?' {
+			wildcards = append(wildcards, i)
+		}
 	}
-	unfolded_sizes := []int{}
-	for i := 0; i < 5; i++ {
-		unfolded_sizes = append(unfolded_sizes, sizes...)
+
+	pos := 0
+	for pos >= 0 {
+		if pos == len(wildcards) {
+			valid++
+			pos--
+		}
+
+		if sequence[wildcards[pos]] == '?' {
+			sequence[wildcards[pos]] = '#'
+			if is_valid(sequence, sizes) {
+				pos++
+				continue
+			}
+		}
+
+		if sequence[wildcards[pos]] == '#' {
+			sequence[wildcards[pos]] = '.'
+			if is_valid(sequence, sizes) {
+				pos++
+				continue
+			}
+		}
+
+		sequence[wildcards[pos]] = '?'
+		pos--
 	}
-	return unfolded_sequence, unfolded_sizes
+
+	return valid
 }
 
 func get_sum(sequence []byte, sizes []int) int {
-	cache = map[CacheKey]int{}
-	return combinations(sequence, sizes, 0, 0, 0)
+	return backtrack(sequence, sizes)
+}
+
+func unfold(sequence []byte, sizes []int) ([]byte, []int) {
+	new_sequence := sequence
+	for i := 0; i < 4; i++ {
+		new_sequence = append(new_sequence, '?')
+		new_sequence = append(new_sequence, sequence...)
+	}
+	new_sizes := []int{}
+	for i := 0; i < 5; i++ {
+		new_sizes = append(new_sizes, sizes...)
+	}
+	return new_sequence, new_sizes
 }
 
 func total_sum(data []string) (int, int) {
 	sum := 0
 	unfolded_sum := 0
+	done := 0
+
+	var wg sync.WaitGroup
 
 	for _, line := range data {
 		parts := strings.Split(line, " ")
@@ -93,18 +142,35 @@ func total_sum(data []string) (int, int) {
 			sizes = append(sizes, n)
 		}
 
-		sum += get_sum(sequence, sizes)
+		wg.Add(1)
 
-		unfolded_sequence, unfolded_sizes := unfold(sequence, sizes)
-		unfolded_sum += get_sum(unfolded_sequence, unfolded_sizes)
+		go func() {
+			defer wg.Done()
+
+			s := get_sum(sequence, sizes)
+			sum += s
+
+			if sequence[0] != '?' && sequence[len(sequence)-1] != '?' {
+				unfolded := int(math.Pow(float64(s), 5) * 16)
+				unfolded_sum += unfolded
+			} else {
+				unfolded_sequence, unfolded_sizes := unfold(sequence, sizes)
+				unfolded_sum += get_sum(unfolded_sequence, unfolded_sizes)
+			}
+
+			done++
+			fmt.Println(done)
+		}()
 	}
+
+	wg.Wait()
 
 	return sum, unfolded_sum
 }
 
 func main() {
 	data := load_data("input.txt")
-	total_sum, total_unfolded_sum := total_sum(data)
-	fmt.Println(total_sum)
-	fmt.Println(total_unfolded_sum)
+	sum, unfolded_sum := total_sum(data)
+	fmt.Println(sum)
+	fmt.Println(unfolded_sum)
 }
