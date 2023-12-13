86bbaaf645a9ddc32e5eab5f2a877106dd04a8f1
David Bell <david.bell@photobox.com>
2023-12-13 10:41:40 +0000

    2023 day 13 part 2

--- 2023/day13/day13.go
+++ 2023/day13/day13.go
@@ -14,19 +14,49 @@ func load_data(filename string) string {
 	return string(data)
 }
 
-func get_notes(data string) int {
+func get_notes(data string) (int, int) {
 	patterns := strings.Split(data, "\n\n")
 
 	total := 0
+	total_second := 0
 	for _, pattern_str := range patterns {
 		lines := strings.Split(pattern_str, "\n")
 		pattern := [][]byte{}
 		for _, line := range lines {
 			pattern = append(pattern, []byte(line))
 		}
-		total += process_pattern(pattern)
+		amount := process_pattern(pattern, -1)
+		total += amount
+		second := find_second_mirror(pattern, amount)
+		total_second += second
 	}
-	return total
+	return total, total_second
+}
+
+func flip(b byte) byte {
+	if b == '#' {
+		return '.'
+	} else {
+		return '#'
+	}
+}
+
+func find_second_mirror(pattern [][]byte, previous_amount int) int {
+	height := len(pattern)
+	width := len(pattern[0])
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			pattern[y][x] = flip(pattern[y][x])
+			amount := process_pattern(pattern, previous_amount)
+			if amount != -1 {
+				return amount
+			}
+			pattern[y][x] = flip(pattern[y][x])
+		}
+	}
+
+	return previous_amount
 }
 
 func is_horizontal_mirror(pattern [][]byte, x int, x2 int) bool {
@@ -61,7 +91,7 @@ func is_vertical_mirror(pattern [][]byte, y int, y2 int) bool {
 	return true
 }
 
-func process_pattern(pattern [][]byte) int {
+func process_pattern(pattern [][]byte, ignore int) int {
 	height := len(pattern)
 	width := len(pattern[0])
 
@@ -69,7 +99,10 @@ func process_pattern(pattern [][]byte) int {
 	for i := 0; i < height-1; i++ {
 		if string(pattern[i]) == string(pattern[i+1]) {
 			if is_horizontal_mirror(pattern, i, i+1) {
-				return (i + 1) * 100
+				note := (i + 1) * 100
+				if note != ignore {
+					return note
+				}
 			}
 		}
 	}
@@ -84,7 +117,10 @@ func process_pattern(pattern [][]byte) int {
 		}
 		if string(col) == string(col2) {
 			if is_vertical_mirror(pattern, i, i+1) {
-				return i + 1
+				note := i + 1
+				if note != ignore {
+					return note
+				}
 			}
 		}
 	}
@@ -92,8 +128,15 @@ func process_pattern(pattern [][]byte) int {
 	return -1
 }
 
+func pp(pattern [][]byte) {
+	for _, row := range pattern {
+		fmt.Println(string(row))
+	}
+}
+
 func main() {
 	data := load_data("input.txt")
-	notes := get_notes(data)
+	notes, notes2 := get_notes(data)
 	fmt.Println(notes)
+	fmt.Println(notes2)
 }
