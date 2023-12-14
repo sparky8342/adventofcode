8433fc355ce1cb7b79a22a44b045d4b3152613fc
David Bell <david.bell@photobox.com>
2023-12-14 14:15:33 +0000

    2023 day 14 part 2

--- 2023/day14/day14.go
+++ 2023/day14/day14.go
@@ -30,9 +30,8 @@ func tilt_north(grid [][]byte) {
 	for y := 1; y < height; y++ {
 		for x := 0; x < width; x++ {
 			if grid[y][x] == 'O' {
-				dx := x
 				dy := y
-				for dy > 0 && grid[dy-1][dx] == '.' {
+				for dy > 0 && grid[dy-1][x] == '.' {
 					dy--
 				}
 				if y != dy {
@@ -41,6 +40,98 @@ func tilt_north(grid [][]byte) {
 				}
 			}
 		}
+	}
+}
+
+func tilt_west(grid [][]byte) {
+	for x := 1; x < width; x++ {
+		for y := 0; y < height; y++ {
+			if grid[y][x] == 'O' {
+				dx := x
+				for dx > 0 && grid[y][dx-1] == '.' {
+					dx--
+				}
+				if x != dx {
+					grid[y][x] = '.'
+					grid[y][dx] = 'O'
+				}
+			}
+		}
+	}
+}
+
+func tilt_south(grid [][]byte) {
+	for y := height - 2; y >= 0; y-- {
+		for x := 0; x < width; x++ {
+			if grid[y][x] == 'O' {
+				dy := y
+				for dy < height-1 && grid[dy+1][x] == '.' {
+					dy++
+				}
+				if y != dy {
+					grid[y][x] = '.'
+					grid[dy][x] = 'O'
+				}
+			}
+		}
+	}
+}
+
+func tilt_east(grid [][]byte) {
+	for x := width - 2; x >= 0; x-- {
+		for y := 0; y < height; y++ {
+			if grid[y][x] == 'O' {
+				dx := x
+				for dx < width-1 && grid[y][dx+1] == '.' {
+					dx++
+				}
+				if x != dx {
+					grid[y][x] = '.'
+					grid[y][dx] = 'O'
+				}
+			}
+		}
+	}
+}
+
+func spin(grid [][]byte) {
+	tilt_north(grid)
+	tilt_west(grid)
+	tilt_south(grid)
+	tilt_east(grid)
+}
+
+func flatten(grid [][]byte) string {
+	str := ""
+	for _, line := range grid {
+		str += string(line)
+	}
+	return str
+}
+
+func spins(grid [][]byte, amount int) {
+	done := 0
+
+	seen := map[string]int{}
+	seen[flatten(grid)] = 0
+
+	var first int
+	for {
+		spin(grid)
+		done++
+		flat := flatten(grid)
+		if value, exists := seen[flat]; exists {
+			first = value
+			break
+		} else {
+			seen[flat] = done
+		}
+	}
+
+	length := done - first
+
+	for i := 0; i < (amount-done)%length; i++ {
+		spin(grid)
 	}
 }
 
@@ -68,5 +159,8 @@ func main() {
 	data := load_data("input.txt")
 	grid := parse_data(data)
 	tilt_north(grid)
+	fmt.Println(get_load(grid))
+
+	spins(grid, 1000000000)
 	fmt.Println(get_load(grid))
 }
--- 2023/day14/day14_test.go
+++ 2023/day14/day14_test.go
@@ -24,4 +24,13 @@ func Test(t *testing.T) {
 	if got_load != want_load {
 		t.Errorf("got %d, wanted %d", got_load, want_load)
 	}
+
+	spins(grid, 1000000000)
+
+	got_load = get_load(grid)
+	want_load = 64
+
+	if got_load != want_load {
+		t.Errorf("got %d, wanted %d", got_load, want_load)
+	}
 }
