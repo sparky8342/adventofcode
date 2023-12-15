b30d20ad779706745d02f648e8705bf409bec6ba
David Bell <david.bell@photobox.com>
2023-12-15 11:23:13 +0000

    2023 day 15 part 1

---
+++ 2023/day15/day15_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func Test(t *testing.T) {
+	data := "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+
+	got_hash_sum := hash_sum(data)
+	want_hash_sum := 1320
+
+	if got_hash_sum != want_hash_sum {
+		t.Errorf("got %d, wanted %d", got_hash_sum, want_hash_sum)
+	}
+}
