3a36a99a4031f7eade7e44738b7a5cf6ab2d3eb0
David Bell <david.bell@gmail.com>
2023-12-16 12:53:40 +0000

    2023 day 16 part 2

--- 2023/day16/day16_test.go
+++ 2023/day16/day16_test.go
@@ -17,10 +17,17 @@ func Test(t *testing.T) {
 	}
 
 	grid := parse_data(data)
-	got_energized := energize(grid)
+	got_energized := energize_top_left(grid)
 	want_energized := 46
 
 	if got_energized != want_energized {
 		t.Errorf("got %d, wanted %d", got_energized, want_energized)
+	}
+
+	got_max_energy := max_energy(grid)
+	want_max_energy := 51
+
+	if got_max_energy != want_max_energy {
+		t.Errorf("got %d, wanted %d", got_max_energy, want_max_energy)
 	}
 }
