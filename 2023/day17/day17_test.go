37360e5d98fb4631799de0db4b4b4719aa2f2c8b
David Bell <david.bell@photobox.com>
2024-01-03 12:34:24 +0000

    2023 day 17 part 1

---
+++ 2023/day17/day17_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func Test(t *testing.T) {
+	data := []string{
+		"2413432311323",
+		"3215453535623",
+		"3255245654254",
+		"3446585845452",
+		"4546657867536",
+		"1438598798454",
+		"4457876987766",
+		"3637877979653",
+		"4654967986887",
+		"4564679986453",
+		"1224686865563",
+		"2546548887735",
+		"4322674655533",
+	}
+
+	got_heat_loss := find_path(data)
+	want_heat_loss := 102
+
+	if got_heat_loss != want_heat_loss {
+		t.Errorf("got %d, wanted %d", got_heat_loss, want_heat_loss)
+	}
+}
