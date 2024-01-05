ec6067c7447b765c1fe9207671b26661f5a39b16
David Bell <david.bell@photobox.com>
2024-01-05 17:51:27 +0000

    2023 day 21 part 2

--- 2023/day21/day21.go
+++ 2023/day21/day21.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"strings"
 )
 
@@ -16,6 +17,8 @@ type State struct {
 	y     int
 	steps int
 }
+
+var height, width int
 
 func load_data(filename string) []string {
 	data, _ := ioutil.ReadFile(filename)
@@ -40,7 +43,14 @@ func find_start(data []string) State {
 	return State{}
 }
 
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
+
 func plots(data []string, amount int) int {
+	height = len(data)
+	width = len(data[0])
+
 	visited := map[State]struct{}{}
 	positions := map[Pos]struct{}{}
 
@@ -73,7 +83,7 @@ func plots(data []string, amount int) int {
 		}
 
 		for _, n := range neighbours {
-			if data[n.y][n.x] == '#' {
+			if data[mod(n.y, height)][mod(n.x, width)] == '#' {
 				continue
 			}
 			for steps := n.steps; steps <= amount; steps += 2 {
@@ -89,8 +99,31 @@ func plots(data []string, amount int) int {
 	return len(positions)
 }
 
+func large_steps(data []string, amount int) *big.Int {
+	a := plots(data, 65)
+	b := plots(data, 65+height)
+	c := plots(data, 65+height*2)
+
+	diff1 := b - a
+	diff2 := c - b
+
+	amount_big := big.NewInt(int64(amount))
+	diff1_big := big.NewInt(int64(diff1))
+	diff2_big := big.NewInt(int64(diff2))
+
+	sum := new(big.Int)
+	sum.Sub(amount_big, big.NewInt(int64(1)))
+	sum.Mul(sum, amount_big)
+	sum.Div(sum, big.NewInt(int64(2)))
+	sum.Mul(sum, new(big.Int).Sub(diff2_big, diff1_big))
+	sum.Add(sum, new(big.Int).Mul(diff1_big, amount_big))
+	sum.Add(sum, big.NewInt(int64(a)))
+
+	return sum
+}
+
 func main() {
 	data := load_data("input.txt")
-	amount := plots(data, 64)
-	fmt.Println(amount)
+	fmt.Println(plots(data, 64))
+	fmt.Println(large_steps(data, 26501365/height))
 }
