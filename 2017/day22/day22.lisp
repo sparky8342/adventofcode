a7231275a0ce5fef15157a7ce24f511fd9d2463f
David Bell <david.bell@photobox.com>
2024-02-01 15:17:04 +0000

    2017 day 22 part 2

--- 2017/day22/day22.lisp
+++ 2017/day22/day22.lisp
@@ -12,6 +12,13 @@
 (defconstant DOWN 3)
 (defconstant LEFT 4)
 
+(defconstant REV 5)
+
+(defconstant CLEAN 1)
+(defconstant WEAKENED 2)
+(defconstant INFECTED 3)
+(defconstant FLAGGED 4)
+
 (defun get-input()
 	(let ((nodehash (make-hash-table :test #'equalp))
 		(y 0)
@@ -26,7 +33,7 @@
 							(let ((node (make-node
 								:x x
 								:y y)))
-								(setf (gethash node nodehash) 0))))
+								(setf (gethash node nodehash) INFECTED))))
 					(setq y (+ y 1))))
 		(values nodehash (/ len 2))))
 
@@ -50,7 +57,17 @@
 			(if (= dir DOWN)
 				(return-from turn LEFT))
 			(if (= dir LEFT)
-				(return-from turn UP)))))
+				(return-from turn UP))))
+	(if (= turndir REV)
+		(progn
+			(if (= dir UP)
+				(return-from turn DOWN))
+			(if (= dir RIGHT)
+				(return-from turn LEFT))
+			(if (= dir DOWN)
+				(return-from turn UP))
+			(if (= dir LEFT)
+				(return-from turn RIGHT)))))
 
 (defun move(carrier dir)
 	(if (= dir UP)
@@ -71,18 +88,47 @@
 		(let ((carrier (make-node
 			 :x middle
 			 :y middle)))
-		(loop for i from 1 to 10000 do
-			(if (gethash carrier nodehash)
-				(progn
-					(remhash carrier nodehash)
-					(setq dir (turn dir RIGHT))
-					(move carrier dir))
-				(progn
-					(setf (gethash (make-node :x (node-x carrier) :y (node-y carrier)) nodehash) 0)
-					(setq infections (+ infections 1))
-					(setq dir (turn dir LEFT))
-					(move carrier dir)))))
+			(loop for i from 1 to 10000 do
+				(if (gethash carrier nodehash)
+					(progn
+						(remhash carrier nodehash)
+						(setq dir (turn dir RIGHT)))
+					(progn
+						(setf (gethash (make-node :x (node-x carrier) :y (node-y carrier)) nodehash) INFECTED)
+						(setq infections (+ infections 1))
+						(setq dir (turn dir LEFT))))
+				(move carrier dir))
+		(princ infections)
+		(terpri))))
+
+(defun part2()
+	(let ((dir UP)
+		(infections 0)
+		(nodehash 0)
+		(middle 0))
+		(setf (values nodehash middle) (get-input))
+		(let ((carrier (make-node
+			 :x middle
+			 :y middle)))
+			(loop for i from 1 to 10000000 do
+				(let ((value (gethash carrier nodehash)))
+					(if (not value)
+						(setq value CLEAN))
+					(when (= value CLEAN)
+						(setf (gethash (make-node :x (node-x carrier) :y (node-y carrier)) nodehash) WEAKENED)
+						(setq dir (turn dir LEFT)))
+					(when (= value WEAKENED)
+						(setf (gethash (make-node :x (node-x carrier) :y (node-y carrier)) nodehash) INFECTED)
+						(setq infections (+ infections 1)))
+					(when (= value INFECTED)
+						(setf (gethash (make-node :x (node-x carrier) :y (node-y carrier)) nodehash) FLAGGED)
+						(setq dir (turn dir RIGHT)))
+					(when (= value FLAGGED)
+						(remhash carrier nodehash)
+						(setq dir (turn dir REV))))
+				(move carrier dir)))
 		(princ infections)
 		(terpri)))
 
 (part1)
+(part2)
