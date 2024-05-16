ae8c55690d5b9124f325e0015d89d738f2ac3e58
David Bell <david.bell@photobox.com>
2024-05-16 15:55:04 +0100

    2019 day16 part2

--- 2019/day16/day16.pl
+++ 2019/day16/day16.pl
@@ -1,15 +1,29 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use List::Util qw(sum);
 
 open my $fh, '<', 'input.txt';
 chomp(my $data = <$fh>);
 my $digits = [split//, $data];
+my $orig_digits = [@$digits];
 
 for (1..100) {
 	$digits = phase($digits);
 }	
 print join('', @$digits[0..7]) . "\n";
+
+
+$digits = [(@$orig_digits) x 10000];
+my $start = join("", @$digits[0..6]);
+
+@$digits = @$digits[$start..@$digits-1];
+
+for (1..100) {
+	$digits = phase2($digits);
+}
+print join('', @$digits[0..7]) . "\n";
+
 
 sub phase {
 	my ($digits) = @_;
@@ -39,5 +53,19 @@ sub phase {
 		$new_digit = substr($new_digit, length($new_digit) - 1, 1);
 		push @new_digits, $new_digit;
 	}
+	return \@new_digits;
+}
+
+sub phase2 {
+	my ($digits, $start) = @_;
+
+	my @new_digits;
+
+	my $sum = sum(@$digits);
+	for (my $i = 0; $i < @$digits; $i++) {
+		push @new_digits, $sum % 10;
+		$sum -= $digits->[$i];
+	}
+
 	return \@new_digits;
 }
