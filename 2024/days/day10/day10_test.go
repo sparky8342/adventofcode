7d96d6488331f21c0adb15dc9e70a2586aeabea8
David Bell <david.bell@gmail.com>
2024-12-10 12:53:26 +0000

    2024 day 10 part 2

--- 2024/days/day10/day10_test.go
+++ 2024/days/day10/day10_test.go
@@ -10,7 +10,7 @@ func Test1(t *testing.T) {
 		"9876",
 	}
 
-	got := score(grid)
+	got, _ := score(grid)
 	want := 1
 
 	if want != got {
@@ -29,7 +29,7 @@ func Test2(t *testing.T) {
 		"9.....9",
 	}
 
-	got := score(grid)
+	got, _ := score(grid)
 	want := 2
 
 	if want != got {
@@ -48,7 +48,7 @@ func Test3(t *testing.T) {
 		"987....",
 	}
 
-	got := score(grid)
+	got, _ := score(grid)
 	want := 4
 
 	if want != got {
@@ -67,7 +67,7 @@ func Test4(t *testing.T) {
 		".....01",
 	}
 
-	got := score(grid)
+	got, _ := score(grid)
 	want := 3
 
 	if want != got {
@@ -87,8 +87,84 @@ func Test5(t *testing.T) {
 		"10456732",
 	}
 
-	got := score(grid)
+	got, _ := score(grid)
 	want := 36
+
+	if want != got {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test6(t *testing.T) {
+	grid := []string{
+		".....0.",
+		"..4321.",
+		"..5..2.",
+		"..6543.",
+		"..7..4.",
+		"..8765.",
+		"..9....",
+	}
+
+	_, got := score(grid)
+	want := 3
+
+	if want != got {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test7(t *testing.T) {
+	grid := []string{
+		"..90..9",
+		"...1.98",
+		"...2..7",
+		"6543456",
+		"765.987",
+		"876....",
+		"987....",
+	}
+
+	_, got := score(grid)
+	want := 13
+
+	if want != got {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test8(t *testing.T) {
+	grid := []string{
+		"012345",
+		"123456",
+		"234567",
+		"345678",
+		"4.6789",
+		"56789.",
+	}
+
+	_, got := score(grid)
+	want := 227
+
+	if want != got {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test9(t *testing.T) {
+	grid := []string{
+		"89010123",
+		"78121874",
+		"87430965",
+		"96549874",
+		"45678903",
+		"32019012",
+		"01329801",
+		"10456732",
+	}
+
+	_, got := score(grid)
+	want := 81
 
 	if want != got {
 		t.Errorf("got %d, wanted %d", got, want)
