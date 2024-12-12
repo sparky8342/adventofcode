16d96a33037edc0096a9bf45e067f752a55e8b39
David Bell <david.bell@gmail.com>
2024-12-12 16:35:11 +0000

    2024 day 12 part 2

--- 2024/days/day12/day12.go
+++ 2024/days/day12/day12.go
@@ -3,14 +3,9 @@ package day12
 import (
 	"fmt"
 	"loader"
+	"sort"
 )
 
-var dirs = [][]int{
-	[]int{0, -1},
-	[]int{1, 0},
-	[]int{0, 1},
-	[]int{-1, 0},
-}
 var size int
 
 type Pos struct {
@@ -18,7 +13,12 @@ type Pos struct {
 	y int
 }
 
-func find_region(grid [][]byte, start_pos Pos) int {
+type Edge struct {
+	x float64
+	y float64
+}
+
+func find_region(grid [][]byte, start_pos Pos) (int, int) {
 	queue := []Pos{start_pos}
 	visited := map[Pos]struct{}{}
 	visited[start_pos] = struct{}{}
@@ -27,29 +27,58 @@ func find_region(grid [][]byte, start_pos Pos) int {
 	region_size := 0
 	perimeter := 0
 
+	vertical_edges := []Edge{}
+	horizontal_edges := []Edge{}
+
 	for len(queue) > 0 {
 		pos := queue[0]
 		queue = queue[1:]
 
 		region_size++
 
-		for _, dir := range dirs {
-			new_x := pos.x + dir[0]
-			new_y := pos.y + dir[1]
-			if new_x < 0 || new_x == size || new_y < 0 || new_y == size {
-				perimeter++
+		neighbours := []Pos{}
+		// left
+		if pos.x == 0 || grid[pos.y][pos.x-1] != plant {
+			vertical_edges = append(vertical_edges, Edge{x: float64(pos.x) - 0.1, y: float64(pos.y)})
+			perimeter++
+		} else {
+			next_pos := Pos{x: pos.x - 1, y: pos.y}
+			neighbours = append(neighbours, next_pos)
+		}
+
+		// right
+		if pos.x == size-1 || grid[pos.y][pos.x+1] != plant {
+			vertical_edges = append(vertical_edges, Edge{x: float64(pos.x) + 0.1, y: float64(pos.y)})
+			perimeter++
+		} else {
+			next_pos := Pos{x: pos.x + 1, y: pos.y}
+			neighbours = append(neighbours, next_pos)
+		}
+
+		// up
+		if pos.y == 0 || grid[pos.y-1][pos.x] != plant {
+			horizontal_edges = append(horizontal_edges, Edge{x: float64(pos.x), y: float64(pos.y) - 0.1})
+			perimeter++
+		} else {
+			next_pos := Pos{x: pos.x, y: pos.y - 1}
+			neighbours = append(neighbours, next_pos)
+		}
+
+		// down
+		if pos.y == size-1 || grid[pos.y+1][pos.x] != plant {
+			horizontal_edges = append(horizontal_edges, Edge{x: float64(pos.x), y: float64(pos.y) + 0.1})
+			perimeter++
+		} else {
+			next_pos := Pos{x: pos.x, y: pos.y + 1}
+			neighbours = append(neighbours, next_pos)
+		}
+
+		for _, neighbour := range neighbours {
+			if _, ok := visited[neighbour]; ok {
 				continue
 			}
-			if grid[new_y][new_x] != plant {
-				perimeter++
-				continue
-			}
-			next_pos := Pos{x: new_x, y: new_y}
-			if _, ok := visited[next_pos]; ok {
-				continue
-			}
-			queue = append(queue, next_pos)
-			visited[next_pos] = struct{}{}
+			queue = append(queue, neighbour)
+			visited[neighbour] = struct{}{}
 		}
 	}
 
@@ -57,10 +86,42 @@ func find_region(grid [][]byte, start_pos Pos) int {
 		grid[pos.y][pos.x] = '.'
 	}
 
-	return region_size * perimeter
+	sort.Slice(vertical_edges, func(i, j int) bool {
+		if vertical_edges[i].x == vertical_edges[j].x {
+			return vertical_edges[i].y < vertical_edges[j].y
+		} else {
+			return vertical_edges[i].x < vertical_edges[j].x
+		}
+	})
+	vertical_sides := 1
+	for i := 0; i < len(vertical_edges)-1; i++ {
+		if vertical_edges[i].x != vertical_edges[i+1].x {
+			vertical_sides++
+		} else if vertical_edges[i].y+1 != vertical_edges[i+1].y {
+			vertical_sides++
+		}
+	}
+
+	sort.Slice(horizontal_edges, func(i, j int) bool {
+		if horizontal_edges[i].y == horizontal_edges[j].y {
+			return horizontal_edges[i].x < horizontal_edges[j].x
+		} else {
+			return horizontal_edges[i].y < horizontal_edges[j].y
+		}
+	})
+	horizontal_sides := 1
+	for i := 0; i < len(horizontal_edges)-1; i++ {
+		if horizontal_edges[i].y != horizontal_edges[i+1].y {
+			horizontal_sides++
+		} else if horizontal_edges[i].x+1 != horizontal_edges[i+1].x {
+			horizontal_sides++
+		}
+	}
+
+	return region_size * perimeter, region_size * (vertical_sides + horizontal_sides)
 }
 
-func price(data []string) int {
+func price(data []string) (int, int) {
 	grid := make([][]byte, len(data))
 	for i, line := range data {
 		grid[i] = []byte(line)
@@ -68,22 +129,24 @@ func price(data []string) int {
 	size = len(grid)
 
 	total := 0
+	discount_total := 0
 	for y := 0; y < size; y++ {
 		for x := 0; x < size; x++ {
 			if grid[y][x] != '.' {
-				total += find_region(grid, Pos{x: x, y: y})
+				t, d := find_region(grid, Pos{x: x, y: y})
+				total += t
+				discount_total += d
 			}
 		}
 	}
-	return total
+	return total, discount_total
 }
 
 func Run() {
 	loader.Day = 12
 	data := loader.GetStrings()
 
-	part1 := price(data)
-	part2 := -1
+	part1, part2 := price(data)
 
 	fmt.Printf("%d %d\n", part1, part2)
 }
