486acab7d4498ddaa98a7f902d7eb5bb3b597caa
David Bell <david.bell@gmail.com>
2024-12-14 00:45:06 +0000

    moved gcd and lcm to utils

--- 2024/utils/utils.go
+++ 2024/utils/utils.go
@@ -7,3 +7,22 @@ func Abs(n int) int {
 		return n
 	}
 }
+
+func Gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func Lcm(a, b int, integers ...int) int {
+	result := a * b / Gcd(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = Lcm(result, integers[i])
+	}
+
+	return result
+}
