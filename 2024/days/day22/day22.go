d19ead458921994bd2a6f5c2ce4edd7922dc7985
David Bell <david.bell@gmail.com>
2024-12-22 14:14:27 +0000

    2024 day 22 part 2

--- 2024/days/day22/day22.go
+++ 2024/days/day22/day22.go
@@ -5,6 +5,8 @@ import (
 	"loader"
 )
 
+const ITERATIONS = 2000
+
 func next(n int) int {
 	n = n ^ (n*64)%16777216
 	n = n ^ (n/32)%16777216
@@ -12,27 +14,80 @@ func next(n int) int {
 	return n
 }
 
-func sequence(n int, amount int) int {
-	for i := 0; i < amount; i++ {
+func sequence(n int) int {
+	for i := 0; i < ITERATIONS; i++ {
 		n = next(n)
 	}
 	return n
 }
 
-func sequences(nums []int, amount int) int {
+func sequences(nums []int) int {
 	total := 0
 	for _, n := range nums {
-		total += sequence(n, amount)
+		total += sequence(n)
 	}
 	return total
+}
+
+func change_sequence(n int) map[[4]int]int {
+	sequences := map[[4]int]int{}
+	last_digit := 0
+
+	sequence := []int{}
+
+	for i := 0; i < ITERATIONS; i++ {
+		n = next(n)
+		digit := n % 10
+		change := digit - last_digit
+		sequence = append(sequence, change)
+
+		if len(sequence) == 5 {
+			sequence = sequence[1:]
+			seq := [4]int{}
+			for j := 0; j < 4; j++ {
+				seq[j] = sequence[j]
+			}
+			if _, ok := sequences[seq]; !ok {
+				sequences[seq] = digit
+			}
+		}
+
+		last_digit = digit
+	}
+
+	return sequences
+}
+
+func best_sequence(nums []int) int {
+	all_sequences := map[[4]int]struct{}{}
+	change_sequences := make([]map[[4]int]int, len(nums))
+	for i, n := range nums {
+		change_sequences[i] = change_sequence(n)
+		for k := range change_sequences[i] {
+			all_sequences[k] = struct{}{}
+		}
+	}
+
+	max := 0
+	for seq := range all_sequences {
+		bananas := 0
+		for _, change_seq := range change_sequences {
+			bananas += change_seq[seq]
+		}
+		if bananas > max {
+			max = bananas
+		}
+	}
+
+	return max
 }
 
 func Run() {
 	loader.Day = 22
 	nums := loader.GetInts()
 
-	part1 := sequences(nums, 2000)
-	part2 := -1
+	part1 := sequences(nums)
+	part2 := best_sequence(nums)
 
 	fmt.Printf("%d %d\n", part1, part2)
 }
--- 2024/days/day22/day22_test.go
+++ 2024/days/day22/day22_test.go
@@ -2,11 +2,22 @@ package day22
 
 import "testing"
 
-var nums = []int{1, 10, 100, 2024}
-
 func Test1(t *testing.T) {
-	got := sequences(nums, 2000)
+	nums := []int{1, 10, 100, 2024}
+
+	got := sequences(nums)
 	want := 37327623
+
+	if want != got {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	nums := []int{1, 2, 3, 2024}
+
+	got := best_sequence(nums)
+	want := 23
 
 	if want != got {
 		t.Errorf("got %d, wanted %d", got, want)
