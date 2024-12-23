e5c45c3d72e6efab33c46fb0ee0a447d127d2819
David Bell <david.bell@gmail.com>
2024-12-23 11:24:52 +0000

    2024 day 23 part 1

---
+++ 2024/days/day23/day23_test.go
@@ -0,0 +1,49 @@
+package day23
+
+import "testing"
+
+var data = []string{
+	"kh-tc",
+	"qp-kh",
+	"de-cg",
+	"ka-co",
+	"yn-aq",
+	"qp-ub",
+	"cg-tb",
+	"vc-aq",
+	"tb-ka",
+	"wh-tc",
+	"yn-cg",
+	"kh-ub",
+	"ta-co",
+	"de-co",
+	"tc-td",
+	"tb-wq",
+	"wh-td",
+	"ta-ka",
+	"td-qp",
+	"aq-cg",
+	"wq-ub",
+	"ub-vc",
+	"de-ta",
+	"wq-aq",
+	"wq-vc",
+	"wh-yn",
+	"ka-de",
+	"kh-ta",
+	"co-tc",
+	"wh-qp",
+	"tb-vc",
+	"td-yn",
+}
+
+var nodes = parse_data(data)
+
+func Test1(t *testing.T) {
+	got := find_three(nodes)
+	want := 7
+
+	if want != got {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
