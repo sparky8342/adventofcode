3c669e6cebcb6a10fcafd68f47ea3f40010b87ce
David Bell[C <david.bell@gmail.com>
2025-12-11 12:37:32 +0000

    2025 day 11 part 2

--- 2025/days/day11/day11.go
+++ 2025/days/day11/day11.go
@@ -11,13 +11,21 @@ type Device struct {
 	links []*Device
 }
 
+type ThroughCacheEntry struct {
+	name string
+	dac  bool
+	fft  bool
+}
+
 var cache map[string]int
+var throughcache map[ThroughCacheEntry]int
 
 func init() {
 	cache = map[string]int{}
+	throughcache = map[ThroughCacheEntry]int{}
 }
 
-func parse_data(data []string) *Device {
+func parse_data(data []string, return_device string) *Device {
 	devices := map[string]*Device{}
 
 	for _, line := range data {
@@ -33,7 +41,7 @@ func parse_data(data []string) *Device {
 		}
 	}
 
-	return devices["you"]
+	return devices[return_device]
 }
 
 func count_paths(node *Device) int {
@@ -54,11 +62,48 @@ func count_paths(node *Device) int {
 	return paths
 }
 
+func count_paths_through(node *Device, dac bool, fft bool) int {
+	key := ThroughCacheEntry{
+		name: node.name,
+		dac:  dac,
+		fft:  fft,
+	}
+
+	if val, ok := throughcache[key]; ok {
+		return val
+	}
+
+	if node.name == "out" {
+		if dac && fft {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	if node.name == "dac" {
+		dac = true
+	} else if node.name == "fft" {
+		fft = true
+	}
+
+	paths := 0
+	for _, link := range node.links {
+		paths += count_paths_through(link, dac, fft)
+	}
+
+	throughcache[key] = paths
+	return paths
+}
+
 func Run() {
 	loader.Day = 11
 	data := loader.GetStrings()
-	you := parse_data(data)
+	you := parse_data(data, "you")
 	part1 := count_paths(you)
 
-	fmt.Printf("%d %d\n", part1, 0)
+	svr := parse_data(data, "svr")
+	part2 := count_paths_through(svr, false, false)
+
+	fmt.Printf("%d %d\n", part1, part2)
 }
--- 2025/days/day11/day11_test.go
+++ 2025/days/day11/day11_test.go
@@ -2,24 +2,51 @@ package day11
 
 import "testing"
 
-var data = []string{
-	"aaa: you hhh",
-	"you: bbb ccc",
-	"bbb: ddd eee",
-	"ccc: ddd eee fff",
-	"ddd: ggg",
-	"eee: out",
-	"fff: out",
-	"ggg: out",
-	"hhh: ccc fff iii",
-	"iii: out",
-}
-
 func Test1(t *testing.T) {
-	you := parse_data(data)
+	data := []string{
+		"aaa: you hhh",
+		"you: bbb ccc",
+		"bbb: ddd eee",
+		"ccc: ddd eee fff",
+		"ddd: ggg",
+		"eee: out",
+		"fff: out",
+		"ggg: out",
+		"hhh: ccc fff iii",
+		"iii: out",
+	}
+
+	you := parse_data(data, "you")
 
 	got := count_paths(you)
 	want := 5
+
+	if want != got {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test2(t *testing.T) {
+	data := []string{
+		"svr: aaa bbb",
+		"aaa: fft",
+		"fft: ccc",
+		"bbb: tty",
+		"tty: ccc",
+		"ccc: ddd eee",
+		"ddd: hub",
+		"hub: fff",
+		"eee: dac",
+		"dac: fff",
+		"fff: ggg hhh",
+		"ggg: out",
+		"hhh: out",
+	}
+
+	svr := parse_data(data, "svr")
+
+	got := count_paths_through(svr, false, false)
+	want := 2
 
 	if want != got {
 		t.Errorf("got %d, wanted %d", got, want)
